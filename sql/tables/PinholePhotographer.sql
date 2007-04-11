de2a91d3404d16e181d6fb98cbe2b157b4a27143
Michael Gauthier <mike@silverorange.com>
2007-04-11 19:29:17 +0000

    respect dataobject changes
    svn commit r16784

--- sql/tables/PinholePhotographer.sql
+++ sql/tables/PinholePhotographer.sql
@@ -1,10 +1,8 @@
 create table PinholePhotographer (
 	id serial,
 	fullname varchar(255),
-	bodytext text,
+	details text,
 	enabled boolean not null default true,
 	archived boolean not null default false,
-	createdate timestamp,
 	primary key (id)
 );
-

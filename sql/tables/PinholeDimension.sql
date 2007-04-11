a4ded4b5c5aca42d5df93c6b8960240d5fa89f1a
Nathan Fredrickson <nathan@silverorange.com>
2007-04-11 18:12:59 +0000

    add some fields
    svn commit r16776

--- sql/tables/PinholeDimension.sql
+++ sql/tables/PinholeDimension.sql
@@ -2,5 +2,8 @@ create table PinholeDimension (
 	id serial,
 	shortname varchar(255),
 	title varchar(255),
+	max_width int,
+	max_height int,
+	crop_to_max boolean default false,
 	primary key (id)
 );

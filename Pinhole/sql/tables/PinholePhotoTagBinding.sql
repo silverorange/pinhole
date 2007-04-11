8a6376138f54b7e76dbcfd799f5fcea42df8770d
Nathan Fredrickson <nathan@silverorange.com>
2007-04-11 18:19:56 +0000

    Remove old sql
    git-svn-id: file:///Users/gauthierm/svnrepo/pinhole/trunk@16778 94490950-a86c-4493-9ccb-530f72933e47

--- Pinhole/sql/tables/PinholePhotoTagBinding.sql
+++
@@ -1,7 +0,0 @@
-create table PinholePhotoTagBinding (
-	photo integer not null references PinholePhoto(id) on delete cascade,
-	tag integer not null references PinholeTag(id) on delete cascade,
-
-	primary key (id)
-);
-

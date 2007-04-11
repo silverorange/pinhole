a3c0e5f09065292d13b2b0a8a49f72752968b8b2
Nathan Fredrickson <nathan@silverorange.com>
2007-04-11 21:30:04 +0000

    Initial image processing.
    git-svn-id: file:///Users/gauthierm/svnrepo/pinhole/trunk@16788 94490950-a86c-4493-9ccb-530f72933e47

--- Pinhole/dataobjects/PinholePhoto.php
+++ Pinhole/dataobjects/PinholePhoto.php
@@ -91,6 +91,115 @@ class PinholePhoto extends SwatDBDataObject
 	public $published;
 
 	// }}}
+	// {{{ protected function getCompressionQuality()
+
+	protected function getCompressionQuality()
+	{
+		return 85;
+	}
+
+	// }}}
+	// {{{ protected function getDpi()
+
+	protected function getDpi()
+	{
+		return 72;
+	}
+
+	// }}}
+
+	// processing methods
+	// {{{ public function processImage()
+
+	/**
+	 * Does resizing for images
+	 *
+	 * @param Image_Transform $transformer the image transformer to work with.
+	 *                                     The tranformer should already have
+	 *                                     an image loaded.
+	 *
+	 * @param PinholeDimension $dimension the dimension to create.
+	 *
+	 * @throws SwatException if no image is loaded in the transformer.
+	 */
+	public function processImage(Image_Transform $transformer,
+		PinholeDimension $dimension)
+	{
+		if ($transformer->image === null)
+			throw new SwatException('No image loaded.');
+
+		if ($dimension->crop_to_max)
+			$this->cropToMax($transformer, $dimension);
+		else
+			$this->fitToMax($transformer, $dimension);
+
+		$dpi = $this->getDpi();
+		$transformer->setDpi($dpi, $dpi);
+
+		if ($dimension->strip)
+			$transformer->strip();
+	}
+
+	// }}}
+	// {{{ private function fitToMax()
+
+	private function fitToMax(Image_Transform $transformer,
+		PinholeDimension $dimension)
+	{
+		if ($dimension->max_width !== null)
+			$transformer->fitX($dimension->max_width);
+
+		if ($dimension->max_height !== null)
+			$transformer->fitY($dimension->max_height);
+	}
+
+	// }}}
+	// {{{ private function cropToMax()
+
+	private function cropToMax(Image_Transform $transformer,
+		PinholeDimension $dimension)
+	{
+		if ($dimension->max_height !== null &&
+			$dimension->max_width !== null) {
+			$height = $dimension->max_height;
+			$width = $dimension->max_width;
+		} elseif ($dimension->max_width !== null) {
+			$height = $dimension->max_width;
+			$width = $dimension->max_width;
+		} elseif ($dimension->max_height !== null) {
+			$height = $dimension->max_height;
+			$width = $dimension->max_height;
+		} else {
+			// nothing to do
+			return;
+		}
+
+		if ($transformer->img_x / $width > $transformer->img_y / $height) {
+			$new_y = $height;
+			$new_x = ceil(($new_y / $transformer->img_y) * $transformer->img_x);
+		} else {
+			$new_x = $width;
+			$new_y = ceil(($new_x / $transformer->img_x) * $transformer->img_y);
+		}
+
+		$transformer->resize($new_x, $new_y);
+
+		// crop to fit
+		if ($transformer->new_x != $width || $transformer->new_y != $height) {
+			$offset_x = 0;
+			$offset_y = 0;
+
+			if ($transformer->new_x > $width)
+				$offset_x = ceil(($transformer->new_x - $width) / 2);
+
+			if ($transformer->new_y > $height)
+				$offset_y = ceil(($transformer->new_y - $height) / 2);
+
+			$transformer->crop($width, $height, $offset_x, $offset_y);
+		}
+	}
+
+	// }}}
 }
 
 ?>

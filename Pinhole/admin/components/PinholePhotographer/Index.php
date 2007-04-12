6da2b555985fd0e1813a5caa0a9e4b8dd959b816
Michael Gauthier <mike@silverorange.com>
2007-04-12 01:34:50 +0000

    Index page for photographers'
    svn commit r16794

---
+++ Pinhole/admin/components/PinholePhotographer/Index.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once 'Admin/pages/AdminIndex.php';
+require_once 'Admin/AdminTableStore.php';
+require_once 'SwatDB/SwatDB.php';
+
+/**
+ * Index page for photographers
+ *
+ * @package   Pinhole
+ * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class PinholePhotographerIndex extends AdminIndex
+{
+	// {{{ protected properties
+
+	protected $ui_xml =
+		'Pinhole/admin/components/PinholePhotographer/index.xml';
+
+	// }}}
+
+	// init phase
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		parent::initInternal();
+		$this->ui->loadFromXML($this->ui_xml);
+	}
+
+	// }}}
+
+	// process phase
+	// {{{ protected function processActions()
+
+	/**
+	 * Processes photographer actions
+	 *
+	 * @param SwatTableView $view the table-view to get selected photographers
+	 *                             from.
+	 * @param SwatActions $actions the actions list widget.
+	 */
+	protected function processActions(SwatTableView $view, SwatActions $actions)
+	{
+		switch ($actions->selected->id) {
+		case 'delete':
+			$this->app->replacePage('PinholePhotographer/Delete');
+			$this->app->getPage()->setItems($view->checked_items);
+			break;
+		}
+	}
+
+	// }}}
+	// {{{ protected function getTableStore()
+
+	/**
+	 * Gets photographer data
+	 *
+	 * @return AdminTableStore photographer information.
+	 */
+	protected function getTableStore($view)
+	{
+		$sql = 'select * from PinholePhotographer
+			order by %s';
+
+		$sql = sprintf($sql,
+			$this->getOrderByClause($view, 'id'));
+		
+		return SwatDB::query($this->app->db, $sql, 'AdminTableStore');
+	}
+
+	// }}}
+}
+
+?>

c80540bedfd97020b59d2d65748ba92d5b9ef021
Nathan Fredrickson <nathan@silverorange.com>
2007-04-13 22:26:05 +0000

    fix component name
    git-svn-id: file:///Users/gauthierm/svnrepo/pinhole/trunk@16826 94490950-a86c-4493-9ccb-530f72933e47

--- sql/admin-changes.sql
+++ sql/admin-changes.sql
@@ -2,7 +2,7 @@ INSERT INTO AdminSection (id, displayorder, title, description, show)
 	VALUES (101, 10, 'Gallery', NULL, true);
 
 INSERT INTO AdminComponent (id, section, displayorder, shortname, title, description, enabled, show)
-	VALUES (100, 101, 10, 'Photographer', 'Photographers', NULL, true, true);
+	VALUES (100, 101, 10, 'PinholePhotographer', 'Photographers', NULL, true, true);
 
 SELECT setval('adminsection_id_seq', max(id)) FROM AdminSection;
 SELECT setval('admincomponent_id_seq', max(id)) FROM AdminComponent;

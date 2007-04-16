9da67966f554583ce81cca8738cebb3374a8ffef
Nick Burka <nick@silverorange.com>
2007-04-16 17:02:17 +0000

    add tools for photos as subcomponents
    git-svn-id: file:///Users/gauthierm/svnrepo/pinhole/trunk@16867 94490950-a86c-4493-9ccb-530f72933e47

--- sql/admin-changes.sql
+++ sql/admin-changes.sql
@@ -4,6 +4,15 @@ INSERT INTO AdminSection (id, displayorder, title, description, show)
 INSERT INTO AdminComponent (id, section, displayorder, shortname, title, description, enabled, show)
 	VALUES (100, 101, 10, 'Photographer', 'Photographers', NULL, true, true);
 
+INSERT INTO AdminComponent (id, section, displayorder, shortname, title, description, enabled, show)
+	VALUES (101, 101, 5, 'Photo', 'Photos', NULL, true, true);
+
+	INSERT INTO AdminSubComponent (id, component, displayorder, shortname, title, show)
+		VALUES (102, 101, 5, 'Upload', 'Upload Photos', true);
+
+	INSERT INTO AdminSubComponent (id, component, displayorder, shortname, title, show)
+		VALUES (101, 101, 10, 'Pending', 'Pending Photos', true);
+
 SELECT setval('adminsection_id_seq', max(id)) FROM AdminSection;
 SELECT setval('admincomponent_id_seq', max(id)) FROM AdminComponent;
 SELECT setval('adminsubcomponent_id_seq', max(id)) FROM AdminSubComponent;

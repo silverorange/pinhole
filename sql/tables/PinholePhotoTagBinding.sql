885039808dc09fc82cc51b22d0d40a2b913e1bf4
Nathan Fredrickson <nathan@silverorange.com>
2007-04-25 19:46:33 +0000

    BOO!!!
    svn commit r17185

--- sql/tables/PinholePhotoTagBinding.sql
+++ sql/tables/PinholePhotoTagBinding.sql
@@ -1,4 +1,4 @@
-create table PineholePhotoTagBinding (
+create table PinholePhotoTagBinding (
 	photo int not null references PinholePhoto(id) on delete cascade,
 	tag int not null references PinholeTag(id) on delete cascade,
 	primary key (photo, tag)

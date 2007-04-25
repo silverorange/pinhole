9892c52a8bce903221e67b963a54a3f6efd065e2
Michael Gauthier <mike@silverorange.com>
2007-04-25 22:10:55 +0000

    Don't show photo processing mockup until uploade is complete.
    svn commit r17192

--- www/admin/javascript/pinhole-photo-upload-page.js
+++ www/admin/javascript/pinhole-photo-upload-page.js
@@ -2,19 +2,12 @@
  * @copyright 2007 silverorange
  */
 
-PinholePhotoUploadPage = function()
+PinholePhotoUploadPage = function(uploader)
 {
 	this.photo_container = document.getElementById('photo_container');
-	var processing_message = document.createElement('h2');
-	var processing_text = document.createTextNode(PinholePhotoUploadPage.processing_text);
-	processing_message.appendChild(processing_text);
-	this.photo_container.parentNode.insertBefore(processing_message, this.photo_container);
+	this.uploader = uploader;
 
-	var clear_div = document.createElement('div');
-	clear_div.style.clear = 'both';
-	this.photo_container.parentNode.appendChild(clear_div);
-
-	this.interval = window.setInterval(function(obj) { obj.addPhoto() }, 4000, this);
+	this.uploader.uploadCompleteEvent.subscribe(this.startDemo, this, true);
 }
 
 PinholePhotoUploadPage.fade_duration = 2;
@@ -40,4 +33,21 @@ PinholePhotoUploadPage.prototype.addPhoto = function()
 		PinholePhotoUploadPage.fade_duration, YAHOO.util.Easing.easeIn);
 
 	animation.animate();
+}
+
+PinholePhotoUploadPage.prototype.startDemo = function()
+{
+	var processing_message = document.createElement('h2');
+	var processing_text = document.createTextNode(PinholePhotoUploadPage.processing_text);
+	processing_message.appendChild(processing_text);
+	this.photo_container.parentNode.insertBefore(processing_message,
+		this.photo_container);
+
+	var clear_div = document.createElement('div');
+	clear_div.style.clear = 'both';
+	this.photo_container.parentNode.appendChild(clear_div);
+
+	this.addPhoto();
+	this.interval = window.setInterval(function(obj) { obj.addPhoto() },
+		4000, this);
 }

c2ee510e5d5cd9db394e140c0be0b2dab594f786
Michael Gauthier <mike@silverorange.com>
2007-04-30 20:35:56 +0000

    Create layout content for subtags.
    svn commit r17265

--- Pinhole/pages/PinholeBrowserPage.php
+++ Pinhole/pages/PinholeBrowserPage.php
@@ -50,6 +50,10 @@ abstract class PinholeBrowserPage extends SitePage
 		$this->layout->startCapture('header_content');
 		$this->displayIntersectingTags();
 		$this->layout->endCapture();
+
+		$this->layout->startCapture('tag_navigation_content');
+		$this->displayNavigationTags();
+		$this->layout->endCapture();
 	}
 
 	// }}}
@@ -71,13 +75,40 @@ abstract class PinholeBrowserPage extends SitePage
 				echo ' <span class="plus">+</span> ';
 
 			$tag_link = new SwatHtmlTag('a');
-			$tag_link->href = $tag->shortname;
+			$tag_link->href = 'tag/'.$tag->shortname;
 			$tag_link->setContent($tag->title);
 			$tag_link->display();
 			$count++;
 		}
 
 		echo '</div>';
+	}
+
+	// }}}
+	// {{{ protected function displayNavigationTags()
+
+	protected function displayNavigationTags()
+	{
+		$tags = $this->tag_intersection->getTags();
+		if (count($tags) == 0)
+			return;
+
+		echo '<ul class="tag-list">';
+
+		foreach ($tags as $tag) {
+			$anchor_tag = new SwatHtmlTag('a');
+			$anchor_tag->setContent($tag->title);
+			$anchor_tag->href = 'tag/'.
+				$this->tag_intersection->getIntersectingTagPath().'/'.
+				$tag->shortname;
+
+			$li_tag = new SwatHtmlTag('li');
+			$li_tag->open();
+			$anchor_tag->display();
+			$li_tag->close();
+		}
+
+		echo '</ul>';
 	}
 
 	// }}}

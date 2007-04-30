d68189d2bcbd96b7271e598af241b43fc7b52de0
Michael Gauthier <mike@silverorange.com>
2007-04-30 20:35:08 +0000

    New methods to get intersecting tag path and to get subtags of current intersecting tags.
    git-svn-id: file:///Users/gauthierm/svnrepo/pinhole/trunk@17263 94490950-a86c-4493-9ccb-530f72933e47

--- Pinhole/PinholeTagIntersection.php
+++ Pinhole/PinholeTagIntersection.php
@@ -71,11 +71,29 @@ class PinholeTagIntersection
 	}
 
 	// }}}
+	// {{{ protected function getIntersectingTagPath()
+
+	public function getIntersectingTagPath()
+	{
+		$path = '';
+		$count = 0;
+		foreach ($this->tags as $tag) {
+			if ($count > 0)
+				$path.= '/';
+
+			$path.= $tag->shortname;
+			$count++;
+		}
+
+		return $path;
+	}
+
+	// }}}
 	// {{{ protected function getPhotos()
 
 	public function getPhotos()
 	{
-		$sql ='select * from PinholePhoto where 1 = 1';
+		$sql = 'select * from PinholePhoto where 1 = 1';
 
 		foreach ($this->tags as $tag) {
 			$sql.= sprintf(' and id in (select photo from '.
@@ -89,6 +107,36 @@ class PinholeTagIntersection
 		$photos = SwatDB::query($this->db, $sql, 'PinholePhotoWrapper');
 
 		return $photos;
+	}
+
+	// }}}
+	// {{{ public function getTags()
+
+	public function getTags()
+	{
+		$sql = 'select * from PinholeTag where id not in (%s) and id in (
+			select tag from PinholePhotoTagBinding where photo in (
+				select id from PinholePhoto where 1 = 1';
+
+		$tag_ids = array();
+		foreach ($this->tags as $tag) {
+			$sql.= sprintf(' and id in (select photo from '.
+				'PinholePhotoTagBinding where tag = %s)',
+				$this->db->quote($tag->id, 'integer'));
+
+			$tag_ids[] = $tag->id;
+		}
+
+		$sql.= '))';
+
+
+		$sql = sprintf($sql,
+			$this->db->implodeArray($tag_ids, 'integer'));
+
+		// TODO: use classmap
+		$tags = SwatDB::query($this->db, $sql, 'PinholeTagWrapper');
+
+		return $tags;
 	}
 
 	// }}}

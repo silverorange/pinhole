85a8f11f9c1bc03b331b5c93be51ba7153b1e767
Michael Gauthier <mike@silverorange.com>
2007-05-01 19:02:02 +0000

    Move gallery layout to pinhole.
    svn commit r17324

---
+++ Pinhole/layouts/xhtml/common/htmlhead.php
@@ -0,0 +1,12 @@
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<base href="<?= $this->basehref ?>" />
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.2.2/build/reset-fonts-grids/reset-fonts-grids.css" />
+	<?=$this->html_head_entries?>
+	<title><?= $this->html_title ?></title>
+	<link rel="icon" href="favicon.ico" type="image/x-icon" />
+	<meta name="description" content="<?= $this->meta_description ?>" />
+	<meta name="ids" content="<?= $this->meta_keywords ?>" />
+	<? /* TODO: add meta keywords and description */ ?> 
+	<!-- Some icons by the Tango project http://www.Tango-Project.org -->
+</head>

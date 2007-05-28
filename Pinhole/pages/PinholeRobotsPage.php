a1e264a8ebfe191a28ed15863c2bf5e3a1d84a1c
Nick Burka <nick@silverorange.com>
2007-05-28 16:05:37 +0000

    Add a customizable robots page
    svn commit r17720

---
+++ Pinhole/pages/PinholeRobotsPage.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once 'Pinhole/pages/PinholePage.php';
+
+/**
+ * @package   Pinhole
+ * @copyright 2007 silverorange
+ */
+class PinholeRobotsPage extends PinholePage
+{
+	// build phase
+	// {{{ public function build()
+
+	public function build()
+	{
+		parent::build();
+
+		echo $this->getDisallowString();
+
+		exit();
+	}
+
+	// }}}
+	// {{{ protected function getDisallowString()
+
+	protected function getDisallowString()
+	{
+		return '';
+		// return "User-agent: * \nDisallow: /";
+	}
+
+	// }}}
+	// {{{ protected function createLayout()
+
+	protected function createLayout()
+	{
+		return new SiteLayout($this->app,
+			'../layouts/xhtml/blank.php');
+	}
+
+	// }}}
+}
+
+?>

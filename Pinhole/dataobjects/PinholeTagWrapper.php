827e79c8443384b251178c3b7cd41fa329978c5c
Michael Gauthier <mike@silverorange.com>
2007-07-10 02:58:38 +0000

    Revert changes to PinholeTagWrapper. This is an experiment in my tree. Something similar may eventually be checked in.
    svn commit r18334

--- Pinhole/dataobjects/PinholeTagWrapper.php
+++ Pinhole/dataobjects/PinholeTagWrapper.php
@@ -1,8 +1,7 @@
 <?php
 
 require_once 'SwatDB/SwatDBRecordsetWrapper.php';
-require_once 'Pinhole/dataobjects/PinholeTagDataObject.php';
-require_once 'Pinhole/tags/PinholeTag.php';
+require_once 'PinholeTag.php';
 
 /**
  * A recordset wrapper class for PinholeTag objects
@@ -11,7 +10,6 @@ require_once 'Pinhole/tags/PinholeTag.php';
  * @copyright 2007 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  * @see       PinholeTag
- * @see       PinholeTagDataObject
  */
 class PinholeTagWrapper extends SwatDBRecordsetWrapper
 {
@@ -20,16 +18,10 @@ class PinholeTagWrapper extends SwatDBRecordsetWrapper
 	protected function init()
 	{
 		parent::init();
-		$this->index_field = 'name';
-	}
 
-	// }}}
-	// {{{ protected function instantiateRowWrapperObject()
-
-	protected function instantiateRowWrapperObject($row)
-	{
-		$data_object = new PinholeTagDataObject($row);
-		return new PinholeTag($data_object);
+		$this->index_field = 'id';
+		$this->row_wrapper_class =
+			$this->class_map->resolveClass('PinholeTag');
 	}
 
 	// }}}

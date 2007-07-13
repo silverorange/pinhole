572c495619f510dd2d2994ed9e4730fb78cd4b56
Nick Burka <nick@silverorange.com>
2007-07-13 21:03:28 +0000

    Correct case
    git-svn-id: file:///Users/gauthierm/svnrepo/pinhole/trunk@18437 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/PinholePhotoMetaDataBinding.sql
+++ sql/tables/PinholePhotoMetaDataBinding.sql
@@ -1,6 +1,6 @@
 create table PinholePhotoMetaDataBinding (
 	photo int not null references PinholePhoto(id) on delete cascade,
-	metadata int not null references PinholeMetaData(id) on delete cascade,
+	meta_data int not null references PinholeMetaData(id) on delete cascade,
 	value varchar(255) not null,
 	primary key (photo, metadata)
 );

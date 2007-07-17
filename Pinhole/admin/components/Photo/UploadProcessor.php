a7ef91b5d9cbe2fdddd8cee57f4edbe42672439f
Nick Burka <nick@silverorange.com>
2007-07-17 00:03:42 +0000

    File processing for UploadProcessor
    git-svn-id: file:///Users/gauthierm/svnrepo/pinhole/trunk@18495 94490950-a86c-4493-9ccb-530f72933e47

--- Pinhole/admin/components/Photo/UploadProcessor.php
+++ Pinhole/admin/components/Photo/UploadProcessor.php
@@ -4,7 +4,6 @@ require_once 'Swat/Swat.php';
 require_once 'Site/pages/SitePage.php';
 require_once 'Admin/exceptions/AdminNotFoundException.php';
 require_once 'Pinhole/dataobjects/PinholePhoto.php';
-require_once 'Pinhole/layouts/PinholePhotoUploadProcessorLayout.php';
 
 /**
  * Page for processing uploaded photos
@@ -42,6 +41,41 @@ class PinholePhotoUploadProcessor extends SitePage
 	public function process()
 	{
 		foreach ($_FILES as $file) {
+			if ($file['name'] !== null) {
+				$ext = strtolower(end(explode('.', $file['name'])));
+
+				$file_path = sprintf('%s/%s.%s',
+					realpath('../../temp/'), uniqid('file'), $ext);
+
+				move_uploaded_file($file['tmp_name'], $file_path);
+				chmod($file_path, 0666);
+
+				if ($ext == 'zip' || $ext == 'tgz' || $ext == 'tar')
+					$files = $this->getArchivedFiles($file);
+				else
+					$files = array($file_path => $file['name']);
+
+				$this->processFiles($files);
+
+				unlink($file_path);
+			}
+
+		}
+	}
+
+	// }}}
+	// {{{ public function processFiles()
+
+	/**
+	 * Processes an array of files
+	 */
+	public function processFiles($files)
+	{
+		foreach ($files as $file => $original_filename) {
+			$photo = new PinholePhoto();
+			$photo->setDatabase($this->app->db);
+			$photo->createFromFile($file, $original_filename);
+			$photo->save();
 		}
 	}
 
@@ -62,9 +96,18 @@ class PinholePhotoUploadProcessor extends SitePage
 
 		Swat::printObject($_POST);
 		Swat::printObject($_FILES);
+
 		Swat::displayInlineJavaScript($this->getInlineJavaScript());
 
 		$this->layout->endCapture();
+	}
+
+	// }}}
+	// {{{ protected function createLayout()
+
+	protected function createLayout()
+	{
+		return new SiteLayout($this->app, 'Pinhole/layouts/PinholePhotoUploadProcessorLayout.php');
 	}
 
 	// }}}
@@ -80,14 +123,6 @@ class PinholePhotoUploadProcessor extends SitePage
 			$javascript.= sprintf("window.parent.%s_obj.complete();\n", $id);
 
 		return $javascript;
-	}
-
-	// }}}
-	// {{{ protected function createLayout()
-
-	protected function createLayout()
-	{
-		return new PinholePhotoUploadProcessorLayout($this->app);
 	}
 
 	// }}}

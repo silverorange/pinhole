fc58880727893c69d03c9f948f7a882ecc6c2fb0
Michael Gauthier <mike@silverorange.com>
2007-07-17 03:25:44 +0000

    Add interface for iterable tags.
    svn commit r18503

---
+++ Pinhole/tags/PinholeIterableTag.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Interface for iterable tags
+ *
+ * Iterable tags are tags that can be incremented and decremented. For example,
+ * date tags can increate and decrease their values.
+ *
+ * @package   Pinhole
+ * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+interface PinholeIterableTag
+{
+	// {{{ public function next()
+
+	/**
+	 * Gets the next tag
+	 *
+	 * @return PinholeAbstractTag the next tag or null if there is no next tag.
+	 */
+	public function next();
+
+	// }}}
+	// {{{ public function prev()
+
+	/**
+	 * Gets the previous tag
+	 *
+	 * @return PinholeAbstractTag the previous tag or null if there is no
+	 *                             previous tag.
+	 */
+	public function prev();
+
+	// }}}
+}
+
+?>

2071e8f7b0926f3d0f1a924f2488d3f54489dc86
Nick Burka <nick@silverorange.com>
2007-07-17 20:40:30 +0000

    Load meta data from photos and add meta data object
    git-svn-id: file:///Users/gauthierm/svnrepo/pinhole/trunk@18544 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Pinhole/dataobjects/PinholePhotoMetaDataWrapper.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once 'SwatDB/SwatDBClassMap.php';
+require_once 'SwatDB/SwatDBRecordsetWrapper.php';
+require_once 'PinholePhoto.php';
+require_once 'PinholePhotoMetaData.php';
+
+/**
+ * A recordset wrapper class for PinholePhoto objects
+ *
+ * @package   Pinhole
+ * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @see       PinholePhoto
+ */
+class PinholePhotoMetaDataWrapper extends SwatDBRecordsetWrapper
+{
+	// {{{ public static function loadSetFromDB()
+
+	/**
+	 * Loads the meta data for a specified photo
+	 *
+	 * @param integer $photo_id
+	 */
+	public static function loadSetFromDB($db, $photo_id)
+	{
+		$sql = 'select PinholePhotoMetaDataBinding.*,
+				PinholeMetaData.title,
+				PinholeMetaData.shortname,
+				PinholeMetaData.machine_tag
+			from PinholePhotoMetaDataBinding
+			inner join PinholeMetaData on
+				PinholeMetaData.id = PinholePhotoMetaDataBinding.meta_data
+			where PinholeMetaData.show = %s
+				and PinholePhotoMetaDataBinding.photo = %s 
+			order by PinholeMetaData.title';
+
+		$sql = sprintf($sql,
+			$db->quote(true, 'boolean'),
+			$db->quote($photo_id, 'integer'));
+
+		$meta_data = SwatDB::query($db, $sql, 'PinholePhotoMetaDataWrapper');
+
+		return $meta_data;
+	}
+
+	// }}}
+	// {{{ protected function init()
+
+	protected function init()
+	{
+		parent::init();
+
+		$this->row_wrapper_class = SwatDBClassMap::get('PinholePhotoMetaData');
+	}
+
+	// }}}
+}
+
+?>

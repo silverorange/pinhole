47260d04ad8eb02de6fb8d57252683f12d7c4197
Michael Gauthier <mike@silverorange.com>
2007-07-19 19:44:15 +0000

    Add subtag list view (needs work to make it as functional as the old implementation).
    git-svn-id: file:///Users/gauthierm/svnrepo/pinhole/trunk@18597 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Pinhole/PinholeSubTagListView.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once 'Swat/SwatControl.php';
+require_once 'Pinhole/Pinhole.php';
+require_once 'Pinhole/PinholeTagList.php';
+
+/**
+ * @package   Pinhole
+ * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class PinholeSubTagListView extends SwatControl
+{
+	public $base = 'tag';
+
+	protected $tag_list;
+	
+	public function __construct($id = null)
+	{
+		parent::__construct($id);
+
+		$this->addStyleSheet(
+			'packages/pinhole/styles/pinhole-sub-tag-list-view.css',
+			Pinhole::PACKAGE_ID);
+	}
+
+	public function display()
+	{
+		if (!$this->visible)
+			return;
+
+		if ($this->tag_list === null)
+			return;
+
+		$sub_tags = $this->tag_list->getSubTags();
+
+		if (count($sub_tags) == 0)
+			return;
+
+
+		$div_tag = new SwatHtmlTag('div');
+		$div_tag->class = 'pinhole-sub-tag-list-view';
+		$div_tag->id = $this->id;
+		$div_tag->open();
+
+		echo '<ul>';
+
+		foreach ($sub_tags as $tag) {
+			$add_list = clone $this->tag_list;
+			$add_list->add($tag);
+
+			echo '<li>';
+
+			$add_anchor_tag = new SwatHtmlTag('a');
+			$add_anchor_tag->class = 'pinhole-tag-list-view-tag';
+			$add_anchor_tag->rel = 'tag';
+//			$add_anchor_tag->title = 
+//				Pinhole::_('View all photos with this tag.');
+
+			$add_anchor_tag->href = $this->base.'/'.$add_list->__toString();
+			$add_anchor_tag->setContent($tag->getTitle());
+			$add_anchor_tag->display();
+
+			echo '</li>';
+
+			unset($add_list);
+		}
+
+		echo '</ul>';
+
+		$div_tag->close();
+	}
+
+	public function setTagList(PinholeTagList $tag_list)
+	{
+		$this->tag_list = $tag_list;
+	}
+}
+
+?>

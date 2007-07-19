960a84322e3a6e6269970f5e10a689e03e143c9e
Michael Gauthier <mike@silverorange.com>
2007-07-19 17:35:36 +0000

    Tag list view for tags at the top of the page.
    git-svn-id: file:///Users/gauthierm/svnrepo/pinhole/trunk@18585 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Pinhole/PinholeTagListView.php
@@ -0,0 +1,86 @@
+<?php
+
+require_once 'Swat/SwatControl.php';
+require_once 'Pinhole/Pinhole.php';
+require_once 'Pinhole/PinholeTagList.php';
+
+/**
+ * @package   Pinhole
+ * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class PinholeTagListView extends SwatControl
+{
+	public $base = 'tag';
+
+	protected $tag_list;
+	
+	public function __construct($id = null)
+	{
+		parent::__construct($id);
+
+		$this->addStyleSheet(
+			'packages/pinhole/styles/pinhole-tag-list-view.css',
+			Pinhole::PACKAGE_ID);
+	}
+
+	public function display()
+	{
+		if (!$this->visible)
+			return;
+
+		if ($this->tag_list === null || count($this->tag_list) == 0)
+			return;
+
+		$div_tag = new SwatHtmlTag('div');
+		$div_tag->class = 'pinhole-tag-list-view';
+		$div_tag->id = $this->id;
+		$div_tag->open();
+
+		$count = 0;
+		foreach ($this->tag_list as $tag) {
+			$remove_list = clone $this->tag_list;
+			$remove_list->remove($tag);
+
+			if ($count > 0)
+				echo '<span class="pinhole-tag-list-view-plus">+</span>';
+
+			echo '<span class="pinhole-tag-list-view-tag-wrapper">';
+
+			$only_anchor_tag = new SwatHtmlTag('a');
+			$only_anchor_tag->class = 'pinhole-tag-list-view-tag';
+			$only_anchor_tag->title =
+				Pinhole::_('View all photos with this tag.');
+
+			$only_anchor_tag->href = $this->base.'/'.$tag->__toString();
+			$only_anchor_tag->setContent($tag->getTitle());
+			$only_anchor_tag->display();
+
+			$remove_anchor_tag = new SwatHtmlTag('a');
+			$remove_anchor_tag->class = 'pinhole-tag-list-view-remove';
+			$remove_anchor_tag->title =
+				Pinhole::_('Remove this tag.');
+
+			$remove_anchor_tag->href =
+				$this->base.'/'.$remove_list->__toString();
+
+			$remove_anchor_tag->setContent('×');
+			$remove_anchor_tag->display();
+
+			echo '</span>';
+
+			unset($remove_list);
+
+			$count++;
+		}
+
+		$div_tag->close();
+	}
+
+	public function setTagList(PinholeTagList $tag_list)
+	{
+		$this->tag_list = $tag_list;
+	}
+}
+
+?>

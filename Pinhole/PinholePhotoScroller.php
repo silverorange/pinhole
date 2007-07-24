2cd3e3cf3eaf63b0391ef08e62b652867ea7c0a2
Michael Gauthier <mike@silverorange.com>
2007-07-24 17:29:16 +0000

    Add widget for scrolling through photos.
    svn commit r18696

---
+++ Pinhole/PinholePhotoScroller.php
@@ -0,0 +1,114 @@
+<?php
+
+require_once 'Pinhole/Pinhole.php';
+require_once 'Pinhole/PinholeTagList.php';
+require_once 'Pinhole/dataobjects/PinholePhoto.php';
+require_once 'Swat/SwatControl.php';
+
+/**
+ * @package   Pinhole
+ * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class PinholePhotoScroller extends SwatControl
+{
+	public $base = 'tag';
+
+	protected $tag_list;
+
+	protected $photo;
+
+	public function display()
+	{
+		if (!$this->visible)
+			return;
+
+		if ($this->tag_list === null || $this->photo === null)
+			return;
+
+		$photos = $this->tag_list->getNextPrevPhotos($this->photo);
+
+		$div_tag = new SwatHtmlTag('div');
+		$div_tag->id = $this->id;
+		$div_tag->class = 'pinhole-photo-scroller';
+		$div_tag->open();
+
+		$this->displayPrev($photos['prev']);
+		$this->displayCurrent($this->photo);
+		$this->displayNext($photos['next']);
+
+		$div_tag->close();
+	}
+
+	public function setTagList(PinholeTagList $tag_list)
+	{
+		$this->tag_list = $tag_list;
+	}
+
+	public function setPhoto(PinholePhoto $photo)
+	{
+		$this->photo = $photo;
+	}
+
+	protected function displayPrev(PinholePhoto $photo = null)
+	{
+		if ($photo === null) {
+			$span_tag = new SwatHtmlTag('span');
+			$span_tag->setContent(Pinhole::_('Prev'));
+			$span_tag->display();
+		} else {
+			$tag_path = (count($this->tag_list) == 0) ?
+				'' : '/'.$this->tag_list->__toString();
+
+			$a_tag = new SwatHtmlTag('a');
+			$a_tag->href = sprintf('photo/%s%s',
+				$photo->id, $tag_path);
+
+			$a_tag->title = $photo->title;
+			$a_tag->class = 'prev';
+			// this is a non-breaking space
+			$a_tag->setContent(Pinhole::_('« Prev'));
+			$a_tag->display();
+		}
+	}
+
+	protected function displayCurrent(PinholePhoto $photo = null)
+	{
+		echo ' ';
+
+		$tag_path = (count($this->tag_list) == 0) ?
+			'' : '/'.$this->tag_list->__toString();
+
+		$a_tag = new SwatHtmlTag('a');
+		$a_tag->setContent(Pinhole::_('View All'));
+		$a_tag->href = 'tag'.$tag_path;
+		$a_tag->class = 'view-all';
+		$a_tag->display();
+
+		echo ' ';
+	}
+
+	protected function displayNext(PinholePhoto $photo = null)
+	{
+		if ($photo === null) {
+			$span_tag = new SwatHtmlTag('span');
+			$span_tag->setContent(Pinhole::_('Next'));
+			$span_tag->display();
+		} else {
+			$tag_path = (count($this->tag_list) == 0) ?
+				'' : '/'.$this->tag_list->__toString();
+
+			$a_tag = new SwatHtmlTag('a');
+			$a_tag->href = sprintf('photo/%s%s',
+				$photo->id, $tag_path);
+
+			$a_tag->title = $photo->title;
+			$a_tag->class = 'next';
+			// this is a non-breaking space
+			$a_tag->setContent(Pinhole::_('Next »'));
+			$a_tag->display();
+		}
+	}
+}
+
+?>

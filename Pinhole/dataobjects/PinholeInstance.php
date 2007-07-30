c3f6a81c41a36483ae4098fd3cf7be7dd50e8e07
Michael Gauthier <mike@silverorange.com>
2007-07-30 21:37:06 +0000

    Add PinholeInstance::loadFromShortname().
    git-svn-id: file:///Users/gauthierm/svnrepo/pinhole/trunk@18853 94490950-a86c-4493-9ccb-530f72933e47

--- Pinhole/dataobjects/PinholeInstance.php
+++ Pinhole/dataobjects/PinholeInstance.php
@@ -42,6 +42,35 @@ class PinholeInstance extends SwatDBDataObject
 	public $enabled;
 
 	// }}}
+	// {{{ public function loadFromShortname()
+
+	/**
+	 * Loads a instance by its shortname
+	 *
+	 * @param string $shortname the shortname of the instance to load.
+	 */
+	public function loadFromName($shortname)
+	{
+		$row = null;
+
+		if ($this->table !== null) {
+			$sql = sprintf('select * from %s where shortname = %s',
+				$this->table,
+				$this->db->quote($shortname, 'text'));
+
+			$rs = SwatDB::query($this->db, $sql, null);
+			$row = $rs->fetchRow(MDB2_FETCHMODE_ASSOC);
+		}
+
+		if ($row === null)
+			return false;
+
+		$this->initFromRow($row);
+		$this->generatePropertyHashes();
+		return true;
+	}
+
+	// }}}
 	// {{{ protected function init()
 
 	protected function init()

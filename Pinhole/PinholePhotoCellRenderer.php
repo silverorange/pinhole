51012d959181acc86810f490815427f7f1f5c7f4
Michael Gauthier <mike@silverorange.com>
2007-07-30 23:39:36 +0000

    Do a todo: Make PinholePhotoCellRenderer encapsulate rather than extend SwatImageCellRenderer.
    svn commit r18873

--- Pinhole/PinholePhotoCellRenderer.php
+++ Pinhole/PinholePhotoCellRenderer.php
@@ -2,6 +2,7 @@
 
 /* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
 
+require_once 'Swat/SwatCellRenderer.php';
 require_once 'Swat/SwatImageCellRenderer.php';
 
 /**
@@ -10,12 +11,17 @@ require_once 'Swat/SwatImageCellRenderer.php';
  * @package   Pinhole
  * @copyright 2007 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
- * @todo      Make this cell renderer encapsulate SwatImageCellRenderer or
- *            SwatImageLinkCellRenderer instead of extending
- *            SwatImageCellRenderer.
  */
-class PinholePhotoCellRenderer extends SwatImageCellRenderer
+class PinholePhotoCellRenderer extends SwatCellRenderer
 {
+	// {{{ class constants
+
+	/**
+	 * Title length before it gets truncated.
+	 */
+	const MAX_TITLE_LENGTH = 30;
+
+	// }}}
 	// {{{ public properties
 
 	/**
@@ -23,9 +29,43 @@ class PinholePhotoCellRenderer extends SwatImageCellRenderer
 	 */
 	public $photo;
 
+	/**
+	 * @var string
+	 */
 	public $link;
 
+	/**
+	 * @var string|array
+	 */
 	public $link_value;
+
+	// }}}
+	// {{{ protected properties
+
+	/**
+	 * @var SwatImageCellRenderer
+	 */
+	protected $image_cell_renderer;
+
+	// }}}
+	// {{{ public function __construct()
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->image_cell_renderer = new SwatImageCellRenderer();
+		$this->image_cell_renderer->parent = $this;
+	}
+
+	// }}}
+	// {{{ public function getHtmlHeadEntrySet()
+
+	public function getHtmlHeadEntrySet()
+	{
+		$set = parent::getHtmlHeadEntrySet();
+		$set->addEntrySet($this->image_cell_renderer->getHtmlHeadEntrySet());
+		return $set;
+	}
 
 	// }}}
 	// {{{ public function render()
@@ -35,36 +75,56 @@ class PinholePhotoCellRenderer extends SwatImageCellRenderer
 		if (!$this->visible)
 			return;
 
+		if ($this->photo->title === null)
+			$title = null;
+		else
+			$title = SwatString::condense($this->photo->title,
+				self::MAX_TITLE_LENGTH);
+
 		if ($this->link !== null) {
 			$a_tag = new SwatHtmlTag('a');
-			$a_tag->href = sprintf($this->link, $this->link_value);
+
+			if ($this->link_value === null)
+				$a_tag->href = $this->link;
+			elseif (is_array($this->link_value))
+				$a_tag->href = vsprintf($this->link, $this->link_value);
+			else
+				$a_tag->href = sprintf($this->link, $this->link_value);
+
 			$a_tag->open();
 		}
 
-		$this->image = $this->getUri();
-		$this->width = $this->photo->getDimension('thumb')->width;
-		$this->height = $this->photo->getDimension('thumb')->height;
-		$this->occupy_width =
+		$this->image_cell_renderer->image = $this->getUri();
+		$this->image_cell_renderer->width =
+			$this->photo->getDimension('thumb')->width;
+
+		$this->image_cell_renderer->height =
+			$this->photo->getDimension('thumb')->height;
+
+		$this->image_cell_renderer->occupy_width =
 			$this->photo->getDimension('thumb')->dimension->max_width;
 
-		$this->occupy_height =
+		$this->image_cell_renderer->occupy_height =
 			$this->photo->getDimension('thumb')->dimension->max_height;
 
-		parent::render();
+		$this->image_cell_renderer->alt = Pinhole::_('Photo Thumbnail.'); 
 
-		$title_tag = new SwatHtmlTag('span');
-		if ($this->photo->title !== null) {
-			$title_tag->setContent(SwatString::condense(
-				$this->photo->title, 30));
-		} else {
-			// Prevent self-closing span tag: <span/>
-			$title_tag->setContent('');
-		}
+		$this->image_cell_renderer->render();
 
-		$title_tag->display();
+		$span_tag = new SwatHtmlTag('span');
+		if ($title === null)
+			$span_tag->setContent(''); // prevent self-closing span tag
+		else
+			$span_tag->setContent($title);
 
-		if ($this->link !== null)
+		if (strlen($this->photo->title) > self::MAX_TITLE_LENGTH)
+			$span_tag->title = $this->photo->title;
+
+		$span_tag->display();
+
+		if ($this->link !== null) {
 			$a_tag->close();
+		}
 	}
 
 	// }}}

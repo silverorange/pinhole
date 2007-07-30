9c7b67ba1e70c5258069569687e2b36ed5691ac3
Michael Gauthier <mike@silverorange.com>
2007-07-30 17:34:32 +0000

    Fix tag details page.
    svn commit r18827

--- Pinhole/admin/components/Tag/Details.php
+++ Pinhole/admin/components/Tag/Details.php
@@ -1,26 +1,34 @@
 <?php
 
-require_once 'Pinhole/dataobjects/PinholeTag.php';
-require_once 'SwatDB/SwatDB.php';
 require_once 'Swat/SwatHtmlTag.php';
 require_once 'Swat/SwatDetailsStore.php';
 require_once 'Swat/SwatNavBar.php';
+require_once 'SwatDB/SwatDB.php';
 require_once 'Admin/pages/AdminIndex.php';
 require_once 'Admin/exceptions/AdminNotFoundException.php';
+require_once 'Pinhole/tags/PinholeTag.php';
 
 /**
- * Details page for Tags
+ * Details page for tags
  *
  * @package   Pinhole
  * @copyright 2005-2007 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
-class PinholeTagDetails extends AdminIndex
+class PinholeTagDetails extends AdminPage
 {
 	// {{{ protected properties
 
 	protected $ui_xml = 'Pinhole/admin/components/Tag/details.xml';
 	protected $id;
+
+	// }}}
+	// {{{ private properties
+
+	/**
+	 * @var PinholeTag
+	 */
+	private $tag;
 
 	// }}}
 
@@ -33,6 +41,24 @@ class PinholeTagDetails extends AdminIndex
 
 		$this->ui->loadFromXML($this->ui_xml);
 		$this->id = SiteApplication::initVar('id');
+
+		$this->initTag();
+	}
+
+	// }}}
+	// {{{ private function initTag()
+
+	private function initTag()
+	{
+		$this->tag = new PinholeTag();
+		$this->tag->setDatabase($this->app->db);
+
+		if (!$this->tag->load($this->id))
+			throw new AdminNotFoundException(sprintf(
+				Pinhole::_('A tag with an id of ‘%d’ does not exist.'),
+				$this->id));
+
+		return $this->tag;
 	}
 
 	// }}}
@@ -43,110 +69,38 @@ class PinholeTagDetails extends AdminIndex
 	protected function buildInternal()
 	{
 		parent::buildInternal();
-		$this->buildTag();
-		$this->buildChildren();
-	}
 
-	// }}}
-	// {{{ protected function getTableModel()
+		$ds = new SwatDetailsStore($this->tag);
+		$ds->photo_count = $this->tag->getPhotoCount();
 
-	protected function getTableModel(SwatView $view)
-	{
-		switch ($view->id) {
-			case 'children_view':
-				return $this->getChildrenTableModel($view);
-		}
-	}
-
-	// }}}
-
-	// build phase - tag details
-	// {{{ private function buildTag()
-
-	private function buildTag()
-	{
-		$tag = $this->loadTag();
-		$ds = new SwatDetailsStore($tag);
 		$details_view = $this->ui->getWidget('details_view');
 		$details_view->data = $ds;
 
 		$details_frame = $this->ui->getWidget('details_frame');
 		$details_frame->title = Pinhole::_('Tag');
-		$details_frame->subtitle = $tag->title;
-		$this->title = $tag->title;
+		$details_frame->subtitle = $this->tag->title;
 
 		$toolbar = $this->ui->getWidget('details_toolbar');
 		$toolbar->setToolLinkValues(array($this->id));
 		$this->ui->getWidget('view_in_gallery')->link =
-			'photos/'.$tag->path;
-		$this->buildNavBar($tag);
+			'photos/'.$this->tag->name;
+
+		$this->buildNavBar();
 	}
 
 	// }}}
+
+	// build phase - tag details
 	// {{{ private function buildNavBar()
 
-	private function buildNavBar($tag)
+	private function buildNavBar()
 	{
 		$this->navbar->popEntry();
+		$this->navbar->addEntry(
+			new SwatNavBarEntry(Pinhole::_('Tags'), 'Tag'));
+
 		$this->navbar->addEntry(new SwatNavBarEntry(
-			Pinhole::_('Tags'), 'Tag'));
-
-		$tag_navbar_rs = SwatDB::executeStoredProc($this->app->db,
-			'getPinholeTagNavbar', array($this->id));
-
-		foreach ($tag_navbar_rs as $entry)
-			if ($entry->id !== $tag->id)
-				$this->navbar->addEntry(new SwatNavBarEntry(
-					$entry->title,
-					'Tag/Details?id='.$entry->id));
-
-		$this->navbar->addEntry(new SwatNavBarEntry($tag->title));
-	}
-
-	// }}}
-	// {{{ private function loadTag()
-
-	private function loadTag()
-	{
-		$tag = new PinholeTag();
-		$tag->setDatabase($this->app->db);
-
-		if (!$tag->load($this->id))
-			throw new AdminNotFoundException(sprintf(
-				Pinhole::_('A tag with an id of ‘%d’ does not exist.'),
-				$this->id));
-
-		return $tag;
-	}
-
-	// }}}
-
-	// build phase - sub-tags
-	// {{{ private function buildChildren()
-
-	private function buildChildren()
-	{
-		$toolbar = $this->ui->getWidget('children_toolbar');
-		$toolbar->setToolLinkValues(array($this->id));
-	}
-
-	// }}}
-	// {{{ private function getChildrenTableModel()
-
-	private function getChildrenTableModel(SwatTableView $view)
-	{
-		$sql = 'select id, title, shortname
-			from PinholeTag
-			where parent = %s
-				and name_space is null
-			order by title';
-
-		$sql = sprintf($sql,
-			$this->app->db->quote($this->id, 'integer'));
-
-		$rs = SwatDB::query($this->app->db, $sql);
-
-		return $rs;
+			$this->tag->title));
 	}
 
 	// }}}

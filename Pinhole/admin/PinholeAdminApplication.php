76cbea490df90daa7a038ccf641c837ecef4a93d
Nick Burka <nick@silverorange.com>
2007-07-30 22:35:59 +0000

    Admin application (will be overridden by gallery)
    git-svn-id: file:///Users/gauthierm/svnrepo/pinhole/trunk@18867 94490950-a86c-4493-9ccb-530f72933e47

---
+++ Pinhole/admin/PinholeAdminApplication.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once 'Pinhole/PinholeMultipleInstanceModule.php';
+require_once 'Admin/AdminApplication.php';
+
+/**
+ * Web application class for an administering Pinhole
+ *
+ * @package   Pinhole
+ * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class PinholeAdminApplication extends AdminApplication
+{
+	// {{{ protected function getDefaultModuleList()
+
+	/**
+	 * Gets the list of default modules to load for this applicaiton
+	 *
+	 * @return array
+	 * @see    AdminApplication::getDefaultModuleList()
+	 */
+	protected function getDefaultModuleList()
+	{
+		$modules = parent::getDefaultModuleList();
+		$modules['instance'] = 'PinholeMultipleInstanceModule'; 
+		return $modules;
+	}
+
+	// }}}
+}
+
+?>

a4e939f9183e353c0580b56051856de5911e11da
Michael Gauthier <mike@silverorange.com>
2007-07-30 20:45:40 +0000

    Add simple admin front-page to Pinhole that redirects to the photo tool.
    svn commit r18841

---
+++ Pinhole/admin/components/Front/Index.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'Admin/pages/AdminPage.php';
+
+/**
+ * Admin front-page that redirects to photos tool
+ *
+ * @package   Pinhole
+ * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class PinholeFrontIndex extends AdminPage
+{
+	// init phase
+	// {{{ protected function initInternal()
+
+	protected function initInternal()
+	{
+		parent::initInternal();
+
+		// relocate to photo index page
+		$this->app->relocate('Photo');
+	}
+
+	// }}}
+}
+
+?>

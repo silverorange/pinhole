f189f43ead5a79dbbcca73b7b80af1816b69ebca
Michael Gauthier <mike@silverorange.com>
2007-07-30 15:00:21 +0000

    Add license tags to dataobjects.
    svn commit r18817

--- Pinhole/dataobjects/PinholeLayouts.php
+++ Pinhole/dataobjects/PinholeLayouts.php
@@ -7,6 +7,7 @@ require_once 'SwatDB/SwatDBDataObject.php';
  *
  * @package   Pinhole
  * @copyright 2007 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class PinholeLayouts extends SwatDBDataObject
 {

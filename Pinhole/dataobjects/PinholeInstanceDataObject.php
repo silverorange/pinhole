ca50f0b9b5f783cb82df553e4042b5f4df2b0ce7
Michael Gauthier <mike@silverorange.com>
2007-07-31 02:12:06 +0000

    Code and documentation cleanups. The delete and save methods need work.
    svn commit r18885

--- Pinhole/dataobjects/PinholeInstanceDataObject.php
+++ Pinhole/dataobjects/PinholeInstanceDataObject.php
@@ -4,11 +4,12 @@ require_once 'SwatDB/SwatDBDataObject.php';
 require_once 'PinholeInstance.php';
 
 /**
- * A specialized dataobject for tables that have PinholeInstance references.
+ * A specialized data-object for objects that are instance-specific
  *
  * @package   Pinhole
  * @copyright 2005-2007 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @see       PinholeInstance
  */
 class PinholeInstanceDataObject extends SwatDBDataObject
 {
@@ -26,9 +27,13 @@ class PinholeInstanceDataObject extends SwatDBDataObject
 	// {{{ public function setInstance()
 
 	/**
-	 * Sets the instance for this data object
+	 * Sets the instance for this data-object
 	 *
-	 * @param PinholeInstance $instance The instance for this dataobject.
+	 * If this data-object has an instance set, subsequent calls to
+	 * {@link PinholeInstanceDataObject::load()} will only load if this object
+	 * belongs to the specified instance.
+	 *
+	 * @param PinholeInstance $instance the instance for this data-object.
 	 */
 	public function setInstance(PinholeInstance $instance)
 	{
@@ -39,34 +44,40 @@ class PinholeInstanceDataObject extends SwatDBDataObject
 	// {{{ protected function loadInternal()
 
 	/**
-	 * Loads this object's properties from the database given an id
+	 * Loads this data-object's properties from the database given an id
+	 *
+	 * If this object is associeted with an instance through the
+	 * {@link PinholeInstanceDataObject::setInstance()} method, load internal
+	 * will only load rows matching the specified instance.
 	 *
 	 * @param mixed $id the id of the database row to set this object's
-	 *               properties with.
+	 *                       properties with.
 	 *
-	 * @return object data row or null.
+	 * @return mixed this object's data row or null is no such row exists.
 	 */
 	protected function loadInternal($id)
 	{
-		if ($this->instance === null)
-			return parent::loadInternal($id);
+		$row = null;
 
-		if ($this->table !== null && $this->id_field !== null) {
-			$id_field = new SwatDBField($this->id_field, 'integer');
-			$sql = 'select * from %s where %s = %s and instance = %s';
+		if ($this->instance === null) {
+			$row = parent::loadInternal($id);
+		} else {
+			if ($this->table !== null && $this->id_field !== null) {
+				$id_field = new SwatDBField($this->id_field, 'integer');
+				$sql = 'select * from %s where %s = %s and instance = %s';
 
-			$sql = sprintf($sql,
-				$this->table,
-				$id_field->name,
-				$this->db->quote($id, $id_field->type),
-				$this->db->quote($this->instance->id, 'integer'));
+				$sql = sprintf($sql,
+					$this->table,
+					$id_field->name,
+					$this->db->quote($id, $id_field->type),
+					$this->db->quote($this->instance->id, 'integer'));
 
-			$rs = SwatDB::query($this->db, $sql, null);
-			$row = $rs->fetchRow(MDB2_FETCHMODE_ASSOC);
-
-			return $row;
+				$rs = SwatDB::query($this->db, $sql, null);
+				$row = $rs->fetchRow(MDB2_FETCHMODE_ASSOC);
+			}
 		}
-		return null;
+
+		return $row;
 	}
 
 	// }}}

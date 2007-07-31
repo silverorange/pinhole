1176263b232e520d4d9f479e048450374f5cbe50
Michael Gauthier <mike@silverorange.com>
2007-07-31 23:14:57 +0000

    Major cleanups to tag entry JavaScript.
    svn commit r18976

--- www/javascript/pinhole-photo-tag-entry.js
+++ www/javascript/pinhole-photo-tag-entry.js
@@ -1,6 +1,5 @@
-
 /**
- * A widget for choosing tags in the photo editor
+ * Control for selecting multiple tags from a list of tags
  *
  * @package   Pinhole
  * @copyright 2007 silverorange
@@ -13,105 +12,156 @@
  * Creates a new tag entry widget
  *
  * @param string id the unique identifier of this entry object.
+ * @param array tag_list an array of tag strings and titles that are possible
+ *                        to select using this control
+ * @param array initial_selected_tag_list an array of already selected tag
+ *                                         strings.
  */
-function PinholePhotoTagEntry(id)
+function PinholePhotoTagEntry(id, tag_list, initial_selected_tag_list)
 {
 	this.id = id;
-	this.selected_tag_array = new Array();
+	this.tag_list = tag_list;
+	this.initial_selected_tag_list = initial_selected_tag_list;
+	this.selected_tag_list = [];
+	this.input_element = document.getElementById(this.id + '_value');
+	this.data_store = new YAHOO.widget.DS_JSArray(this.tag_list);
+	this.list_element = document.getElementById(this.id + '_list');
 
 	YAHOO.util.Event.onContentReady(
-		this.id, this.handleOnAvailable, this, true);
+		this.id + '_value', this.handleOnAvailable, this, true);
 }
 
 // }}}
 // {{{ handleOnAvailable()
 
 /**
- * Sets up the cropping widget when the cropper is available and loaded in the
- * DOM tree
+ * Sets up the auto-complete widget used by this tag selection control
  */
 PinholePhotoTagEntry.prototype.handleOnAvailable = function()
 {
-	this.oACDS = new YAHOO.widget.DS_JSArray(this.tag_array);
- 
-	// Instantiate AutoComplete 
-	autocomplete = new YAHOO.widget.AutoComplete(this.id, this.id + '_container', this.oACDS);
-	autocomplete.queryDelay = 0; 
-	autocomplete.minQueryLength = 0; 
-	autocomplete.highlightClassName = 'pinhole-photo-tag-highlight';
-	autocomplete.prehighlightClassName = "pinhole-photo-tag-prehighlight"; 
-	//autocomplete.useShadow = true; 
-	autocomplete.forceSelection = true;
-	autocomplete.formatResult = function(oResultItem, sQuery) { 
-		var sMarkup = oResultItem[0] + " (" + oResultItem[1] + ")"; 
-		return (sMarkup);
-	}
+	// create auto-complete widget
+	var auto_complete = new YAHOO.widget.AutoComplete(
+		this.input_element, this.id + '_container', this.data_store, {
+		queryDelay:            0,
+		minQueryLength:        0,
+		highlightClassName:    'pinhole-photo-tag-highlight',
+		prehighlightClassName: 'pinhole-photo-tag-prehighlight',
+		useShadow:             false,
+		forceSelection:        true,
+		animVert:              false,
+		formatResult:
+			function(item, query)
+			{
+				// 0 is shortname, 1 is title
+				return item[0] + ' (' + item[1] + ')'; 
+			}
+	});
 
-	autocomplete.itemSelectEvent.subscribe(
+	auto_complete.itemSelectEvent.subscribe(
 		this.addTagFromAutoComplete, this, true);
 
-	// init values passed in
-	if (this.value_array.length > 0)
-		for (var i = 0; i < this.value_array.length; i++)
-			this.addTag(this.value_array[i]);
+	// initialize values passed in
+	for (var i = 0; i < this.initial_selected_tag_list.length; i++)
+		this.addTag(this.initial_selected_tag_list[i]);
 }
 
 // }}}
 // {{{ addTagFromAutoComplete()
 
-PinholePhotoTagEntry.prototype.addTagFromAutoComplete = function(oSelf , elItem , oData)
+PinholePhotoTagEntry.prototype.addTagFromAutoComplete = function(
+	oSelf, elItem, oData)
 {
-	var shortname = elItem[2][1];
-	this.addTag(shortname);
+	var tag_string = elItem[2][1];
+	this.addTag(tag_string);
 }
 
 // }}}
 // {{{ addTag()
 
-PinholePhotoTagEntry.prototype.addTag = function(shortname)
+PinholePhotoTagEntry.prototype.addTag = function(tag_string)
 {
-	var title = null;
+	var found = false;
 
-	for (i = 0; i < this.oACDS.data.length; i++) {
-		if (this.oACDS.data[i][1] == shortname) {
-			title = this.oACDS.data[i][0];
-			var element = this.oACDS.data.splice(i, 1);
-			this.selected_tag_array.push(element[0]);
+	for (i = 0; i < this.data_store.data.length; i++) {
+		if (this.data_store.data[i][1] == tag_string) {
+			// get tag title
+			var title = this.data_store.data[i][0];
+
+			// remove row from data store
+			var element = this.data_store.data.splice(i, 1);
+
+			// add row to list of selected tags, splice returns an array
+			this.selected_tag_list.push(element[0]);
+
+			found = true;
+			break;
 		}
 	}
 
-	if (title == null)
+	if (!found)
 		return;
 
+	// create new list node
 	var li_tag = document.createElement('li');
-	li_tag.id = this.id + '_tag_' + shortname;
-	li_tag.innerHTML = title + " (<a href=\"javascript: " + this.id  + "_obj.removeTag('" + shortname + "')\">remove</a>)";
+	li_tag.id = this.id + '_tag_' + tag_string;
+
+	var title_node = document.createTextNode(title + ' ');
+
+	var anchor_tag = document.createElement('a');
+	anchor_tag.id = this.id + '_tag_remove_' + tag_string;
+	anchor_tag.href = '#';
+	anchor_tag.appendChild(document.createTextNode(
+		PinholePhotoTagEntry.remove_text));
+
+	YAHOO.util.Event.addListener(anchor_tag, 'click',
+		function (e)
+		{
+			YAHOO.util.Event.preventDefault(e);
+			this.removeTag(tag_string);
+		},
+		this, true);
 
 	var hidden_tag = document.createElement('input');
 	hidden_tag.type = 'hidden';
 	hidden_tag.name = this.id + '[]';
-	hidden_tag.value = shortname;
+	hidden_tag.value = tag_string;
 
+	li_tag.appendChild(title_node);
+	li_tag.appendChild(anchor_tag);
 	li_tag.appendChild(hidden_tag);
-	document.getElementById(this.id + '_list').appendChild(li_tag);
+
+	// add list node
+	this.list_element.appendChild(li_tag);
 
 	// clear input value once a value is chosen
-	document.getElementById(this.id).value = '';
+	this.input_element.value = '';
 }
 
 // }}}
 // {{{ removeTag()
 
-PinholePhotoTagEntry.prototype.removeTag = function(shortname)
+PinholePhotoTagEntry.prototype.removeTag = function(tag_string)
 {
-	var li_tag = document.getElementById(this.id + '_tag_' + shortname);
-	document.getElementById(this.id + '_list').removeChild(li_tag);	
+	// remove event listener
+	var anchor_tag = document.getElementById(
+		this.id + '_tag_remove_' + tag_string);
 
-	for (i = 0; i < this.selected_tag_array.length; i++) {
-		if (this.selected_tag_array[i][1] == shortname) {
-			var element = this.selected_tag_array.splice(i, 1);
-			this.oACDS.data.push(element[0]);
-			this.oACDS.data.sort();
+	if (anchor_tag)
+		YAHOO.util.Event.purgeElement(anchor_tag);
+
+	// remove list node 
+	var li_tag = document.getElementById(this.id + '_tag_' + tag_string);
+	if (li_tag)
+		li_tag.parentNode.removeChild(li_tag);
+
+	for (i = 0; i < this.selected_tag_list.length; i++) {
+		if (this.selected_tag_list[i][1] == tag_string) {
+			// remove row from selected list
+			var element = this.selected_tag_list.splice(i, 1);
+
+			// add row back to data store, splice returns an array
+			this.data_store.data.push(element[0]);
+			this.data_store.data.sort();
 			break;
 		}
 	}
@@ -121,30 +171,10 @@ PinholePhotoTagEntry.prototype.removeTag = function(shortname)
 // {{{ static properties
 
 /**
- * Tags
+ * Remove string resource
  *
- * An array of tag values to use for autocomplete 
- *
- * @var array
+ * @var string
  */
-PinholePhotoTagEntry.tag_array;
-
-/**
- * Values
- *
- * An array of tags that are added at startup 
- *
- * @var array
- */
-PinholePhotoTagEntry.value_array;
-
-/**
- * Selected Tags
- *
- * An array of tag values selected in the widget 
- *
- * @var array
- */
-PinholePhotoTagEntry.selected_tag_array;
+PinholePhotoTagEntry.remove_text = 'remove';
 
 // }}}

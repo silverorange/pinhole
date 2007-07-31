5f29a78438cba3c9538c8353cd04bc5f1f11c8d2
Nathan Fredrickson <nathan@silverorange.com>
2007-07-31 19:04:42 +0000

    set serial sequence
    svn commit r18948

--- sql/tables/PinholeDimension.sql
+++ sql/tables/PinholeDimension.sql
@@ -13,6 +13,10 @@ create table PinholeDimension (
 
 CREATE INDEX PinholeDimension_instance_index ON PinholeDimension(instance);
 
-INSERT INTO PinholeDimension (id, shortname, title, max_width, max_height, crop_to_max, strip, publicly_accessible, instance) VALUES (2, 'thumb', 'Thumbnail', 100, 100, false, true, true, 1);
-INSERT INTO PinholeDimension (id, shortname, title, max_width, max_height, crop_to_max, strip, publicly_accessible, instance) VALUES (3, 'large', 'Large', 800, 800, false, true, false, 1);
+INSERT INTO PinholeDimension (id, shortname, title, max_width, max_height, crop_to_max, strip, publicly_accessible, instance)
+VALUES (1, 'thumb', 'Thumbnail', 100, 100, false, true, true, 1);
 
+INSERT INTO PinholeDimension (id, shortname, title, max_width, max_height, crop_to_max, strip, publicly_accessible, instance)
+VALUES (2, 'large', 'Large', 800, 800, false, true, false, 1);
+
+SELECT setval('pinholedimension_id_seq', max(id)) FROM PinholeDimension;

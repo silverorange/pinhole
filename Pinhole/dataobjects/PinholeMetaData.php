0219eb4a95a53c577d6608185a967397c1561e18
Michael Gauthier <mike@silverorange.com>
2007-07-31 16:33:35 +0000

    Add PinholeMetaData::loadFromShortname()
    svn commit r18925

--- Pinhole/dataobjects/PinholeMetaData.php
+++ Pinhole/dataobjects/PinholeMetaData.php
@@ -49,6 +49,53 @@ class PinholeMetaData extends PinholeInstanceDataObject
 	public $machine_tag;
 
 	// }}}
+	// {{{ public function loadFromShortname()
+
+	/**
+	 * Loads a meta-data data-object by its shortname
+	 *
+	 * If this object is associeted with an instance through the
+	 * {@link PinholeInstanceDataObject::setInstance()} method,
+	 * loadFromShortname() will only load rows matching the specified instance.
+	 *
+	 * @param string $shortname the shortname of the meta-data data-object to
+	 *                           load.
+	 *
+	 * @return boolean true if this meta-data data-object was loaded and false
+	 *                  if it could not be loaded.
+	 */
+	public function loadFromShortname($shortname)
+	{
+		$row = null;
+		$loaded = false;
+
+		if ($this->table !== null) {
+			if ($this->instance === null) {
+				$sql = sprintf('select * from %s where shortname = %s',
+					$this->table,
+					$this->db->quote($shortname, 'text'));
+			} else {
+				$sql = sprintf('select * from %s where shortname = %s
+					and instance = %s',
+					$this->table,
+					$this->db->quote($shortname, 'text'),
+					$this->db->quote($this->instance->id, 'integer'));
+			}
+
+			$rs = SwatDB::query($this->db, $sql, null);
+			$row = $rs->fetchRow(MDB2_FETCHMODE_ASSOC);
+		}
+
+		if ($row !== null) {
+			$this->initFromRow($row);
+			$this->generatePropertyHashes();
+			$loaded = true;
+		}
+
+		return $loaded;
+	}
+
+	// }}}
 	// {{{ protected function init()
 
 	protected function init()

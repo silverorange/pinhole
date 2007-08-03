110988c014305339d43e4c0aaf3e99a7a25d7cfa
Nathan Fredrickson <nathan@silverorange.com>
2007-08-03 19:58:51 +0000

    #1183 slashes in metadata values need to escaped and unescaped
    svn commit r19137

--- Pinhole/dataobjects/PinholePhotoMetaDataBinding.php
+++ Pinhole/dataobjects/PinholePhotoMetaDataBinding.php
@@ -50,6 +50,62 @@ class PinholePhotoMetaDataBinding extends SwatDBDataObject
 	public $machine_tag;
 
 	// }}}
+	// {{{ public function getURI()
+
+	/**
+	 * Gets the URI snippet
+	 *
+	 * @return string a URI tag snippet for this meta data binding.
+	 */
+	public function getURI()
+	{
+		$value = self::escapeValue($this->value);
+		$value = rawurlencode($value);
+
+		$uri = sprintf('meta.%s=%s',
+			$this->shortname,
+			$value);
+
+		return $uri;
+	}
+
+	// }}}
+	// {{{ public static function escapeValue()
+
+	/**
+	 * Escapes a meta data value for inclusion in a tag list URI
+	 *
+	 * @param string $string the value to be escaped.
+	 *
+	 * @return string the escaped value.
+	 */
+	public static function escapeValue($string)
+	{
+		$string = str_replace('|', '||', $string);
+		$string = str_replace('/', '|s', $string);
+
+		return $string;
+	}
+
+	// }}}
+	// {{{ public static function unescapeValue()
+
+	/**
+	 * Unescapes a meta data value after inclusion in a tag list URI
+	 *
+	 * @param string $string the value to be unescaped.
+	 *
+	 * @return string the unescaped value.
+	 */
+	public static function unescapeValue($string)
+	{
+		$string = str_replace('|s', '/', $string);
+		$string = str_replace('||', '|', $string);
+
+		return $string;
+	}
+
+	// }}}
 	// {{{ protected function init()
 
 	protected function init()

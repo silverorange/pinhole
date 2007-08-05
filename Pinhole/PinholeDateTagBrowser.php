8a13a925565e0fad34d7bd6bd9dff5a180dac1a2
Michael Gauthier <mike@silverorange.com>
2007-08-05 05:09:49 +0000

    Cleanup, folding and documentation for date tag browser. I like the new sideways browsing function Nathan.
    svn commit r19152

--- Pinhole/PinholeDateTagBrowser.php
+++ Pinhole/PinholeDateTagBrowser.php
@@ -2,20 +2,64 @@
 
 require_once 'Swat/SwatControl.php';
 require_once 'Pinhole/PinholeTagList.php';
+require_once 'MDB2.php';
 
 /**
+ * A control to browse photos by date tags
+ *
+ * Given a tag list, this control displays tags for the applicable years,
+ * months and days of the photo set belonging to the tag list.
+ *
  * @package   Pinhole
  * @copyright 2007 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ * @see       PinholeDateTag
  */
 class PinholeDateTagBrowser extends SwatControl
 {
+	// {{{ public properties
+
+	/**
+	 * The URI component relative the base URI used for displaying links
+	 *
+	 * This value will be prepended to links displayed by this date tag
+	 * browser.
+	 *
+	 * @var string
+	 */
 	public $base = 'tag';
 
+	// }}}
+	// {{{ protected properties
+
+	/**
+	 * The tag list of this date tag browser
+	 *
+	 * @var PinholeTagList
+	 *
+	 * @see PinholeDateTagBrowser::setTagList()
+	 */
 	protected $tag_list;
-	protected $instance;
+
+	/**
+	 * The database connection used by this date tag browser
+	 *
+	 * @var MDB2_Driver_Common
+	 *
+	 * @see PinholeDateTagBrowser::setDatabase()
+	 */
 	protected $db;
 
+	// }}}
+	// {{{ public function __construct()
+
+	/**
+	 * Creates a new date tag browser control
+	 *
+	 * @param string $id a non-visible unique id for this widget.
+	 *
+	 * @see SwatWidget::__construct()
+	 */
 	public function __construct($id = null)
 	{
 		parent::__construct($id);
@@ -25,6 +69,12 @@ class PinholeDateTagBrowser extends SwatControl
 			Pinhole::PACKAGE_ID);
 	}
 
+	// }}}
+	// {{{ public function display()
+
+	/**
+	 * Displays this date tag browser
+	 */
 	public function display()
 	{
 		if (!$this->visible)
@@ -60,21 +110,49 @@ class PinholeDateTagBrowser extends SwatControl
 		$div_tag->close();
 	}
 
+	// }}}
+	// {{{ public function setTagList()
+
+	/**
+	 * Sets the tag list used by this date tag browser
+	 *
+	 * @param PinholeTagList $tag_list the tag list to use for this date tag
+	 *                                  browser.
+	 */
 	public function setTagList(PinholeTagList $tag_list)
 	{
 		$this->tag_list = $tag_list;
 	}
 
+	// }}}
+	// {{{ public function setDatabase()
+
+	/**
+	 * Sets the database connection used by this date tag browser
+	 *
+	 * @param MDB2_Driver_Common $db the database connection to use for this
+	 *                                date tag browser.
+	 */
 	public function setDatabase(MDB2_Driver_Common $db)
 	{
 		$this->db = $db;
 	}
 
-	public function setInstance(PinholeInstance $instance)
-	{
-		$this->instance = $instance;
-	}
+	// }}}
+	// {{{ protected function displayYears()
 
+	/**
+	 * Displays date.year tags for all the photos in the site instance of
+	 * this date tag browser's tag list
+	 *
+	 * The <i>$start_date</i> and <i>$end_date</i> are used to tell which
+	 * year is currently selected.
+	 *
+	 * @param SwatDate $start_date the start date of the photos in this date
+	 *                              tag browser's tag list.
+	 * @param SwatDate $end_date the end date of the photos in this date
+	 *                              tag browser's tag list.
+	 */
 	protected function displayYears(SwatDate $start_date, SwatDate $end_date)
 	{
 		// create a new tag list with the same filters as the current tag
@@ -93,13 +171,21 @@ class PinholeDateTagBrowser extends SwatControl
 		$year_end   = $global_date_range['end']->getYear();
 		$date       = new SwatDate();
 
+		// get selected year if it exists
+		if ($start_date->getYear() == $end_date->getYear())
+			$selected_year = $start_date->getYear();
+		else
+			$selected_year = null;
+
+		// create base tag list that new date.year tags will be added to
+		$tag_list = $this->tag_list->filter(array('PinholeDateTag'));
+		$photos = $this->getPhotoCountByDate($tag_list, 'year');
+
+		// display date.year tags for each year in global date range
 		$div_tag = new SwatHtmlTag('div');
 		$div_tag->class = 'years clearfix';
 		$div_tag->open();
 
-		$tag_list = $this->tag_list->filter(array('PinholeDateTag'));
-
-		$photos = $this->getPhotoCountByDate('year', $tag_list);
 		for ($i = $year_start; $i <= $year_end; $i++) {
 			$date->setYear($i);
 
@@ -113,10 +199,9 @@ class PinholeDateTagBrowser extends SwatControl
 					'one photo', '%s photos', $photos[$i]),
 					SwatString::numberFormat($photos[$i]));
 
-				$a_tag->href = 'tag?'.$tag_list->__toString();
+				$a_tag->href = $this->base.'?'.$tag_list->__toString();
 
-				if ($start_date->getYear() == $end_date->getYear() &&
-					$start_date->getYear() == $i) {
+				if ($selected_year === $i) {
 					$a_tag->class = 'selected';
 				}
 
@@ -134,20 +219,50 @@ class PinholeDateTagBrowser extends SwatControl
 		$div_tag->close();
 	}
 
+	// }}}
+	// {{{ protected function displayMonths()
+
+	/**
+	 * Displays date.month tags for all the photos in the site instance of
+	 * this date tag browser's tag list
+	 *
+	 * The <i>$start_date</i> and <i>$end_date</i> are used to tell which
+	 * month and year are currently selected.
+	 *
+	 * @param SwatDate $start_date the start date of the photos in this date
+	 *                              tag browser's tag list.
+	 * @param SwatDate $end_date the end date of the photos in this date
+	 *                              tag browser's tag list.
+	 */
 	protected function displayMonths(SwatDate $start_date, SwatDate $end_date)
 	{
-
 		$date = new SwatDate();
+
+		/*
+		 * Setting the year to the $start_date year makes sense because the
+		 * month list is only displayed if the start year and end year are the
+		 * same.
+		 */
 		$date->setYear($start_date->getYear());
 
+		// get selected month if it exists
+		if ($start_date->getMonth() == $end_date->getMonth()) {
+			// use intval() to fix a PEAR::Date bug
+			$selected_month = intval($start_date->getMonth());
+		} else {
+			$selected_month = null;
+		}
+
+		// create base tag list that new date.month tags will be added to
+		$tag_list = $this->tag_list->filter(array('PinholeDateTag'));
+		$tag_list->add(sprintf('date.year=%s', $date->format('%Y')));
+		$photos = $this->getPhotoCountByDate($tag_list, 'month');
+
+		// display date.month tags for each month
 		$div_tag = new SwatHtmlTag('div');
 		$div_tag->class = 'months clearfix';
 		$div_tag->open();
 
-		$tag_list = $this->tag_list->filter(array('PinholeDateTag'));
-		$tag_list->add(sprintf('date.year=%s', $date->format('%Y')));
-
-		$photos = $this->getPhotoCountByDate('month', $tag_list);
 		for ($i = 1; $i <= 12; $i++) {
 			$date->setMonth($i);
 			$key = $date->format('%Y-%m');
@@ -161,12 +276,11 @@ class PinholeDateTagBrowser extends SwatControl
 					'one photo', '%s photos', $photos[$key]),
 					SwatString::numberFormat($photos[$key]));
 
-				if ($start_date->getMonth() == $end_date->getMonth() &&
-					$start_date->getMonth() == $i) {
+				if ($selected_month === $i) {
 					$a_tag->class = 'selected';
 				}
 
-				$a_tag->href = 'tag?'.$tag_list->__toString();
+				$a_tag->href = $this->base.'?'.$tag_list->__toString();
 
 				$a_tag->setContent($date->format('%B'));
 				$a_tag->display();
@@ -182,25 +296,58 @@ class PinholeDateTagBrowser extends SwatControl
 		$div_tag->close();
 	}
 
+	// }}}
+	// {{{ protected function displayDays()
+
+	/**
+	 * Displays date.date tags for all the photos in the site instance of
+	 * this date tag browser's tag list
+	 *
+	 * The <i>$start_date</i> and <i>$end_date</i> are used to tell which
+	 * day, month and year are currently selected.
+	 *
+	 * @param SwatDate $start_date the start date of the photos in this date
+	 *                              tag browser's tag list.
+	 * @param SwatDate $end_date the end date of the photos in this date
+	 *                              tag browser's tag list.
+	 */
 	protected function displayDays(SwatDate $start_date, SwatDate $end_date)
 	{
-
 		$date = new SwatDate();
-		$date->setMonth($start_date->getMonth());
-		$date->setYear($end_date->getYear());
 
+		/*
+		 * Setting the month and year to the $start_date month and year makes
+		 * sense because the day list is only displayed if the start year and
+		 * month are the same as the end year and month.
+		 */
+		$date->setMonth($start_date->getMonth());
+		$date->setYear($start_date->getYear());
+
+		// get selected day if it exists
+		if ($start_date->getDay() == $end_date->getDay()) {
+			// use intval() to fix a PEAR::Date bug
+			$selected_day = intval($start_date->getDay());
+		} else {
+			$selected_day = null;
+		}
+
+		// create base tag list that new date.month tags will be added to
+		$tag_list = $this->tag_list->filter(array('PinholeDateTag'));
+		$tag_list->add(sprintf('date.year=%s', $date->format('%Y')));
+		$tag_list->add(sprintf('date.month=%s', $date->format('%m')));
+		$photos = $this->getPhotoCountByDate($tag_list, 'day');
+
+		// Filter again since the day list uses date.date tags instead of
+		// combined date.year, date.month and date.day tags.
+		$tag_list = $this->tag_list->filter(array('PinholeDateTag'));
+
+		// display date.date tags for each day
 		$div_tag = new SwatHtmlTag('div');
 		$div_tag->class = 'days clearfix';
 		$div_tag->open();
 
-		$tag_list = $this->tag_list->filter(array('PinholeDateTag'));
-		$tag_list->add(sprintf('date.year=%s', $date->format('%Y')));
-		$tag_list->add(sprintf('date.month=%s', $date->format('%m')));
-		$photos = $this->getPhotoCountByDate('day', $tag_list);
-		// filter again to use to make the links
-		$tag_list = $this->tag_list->filter(array('PinholeDateTag'));
-
-		for ($i = 1; $i <= $date->getDaysInMonth(); $i++) {
+		$days_in_month = $date->getDaysInMonth();
+		for ($i = 1; $i <= $days_in_month; $i++) {
 			$date->setDay($i);
 			$key = $date->format('%Y-%m-%d');
 
@@ -213,12 +360,11 @@ class PinholeDateTagBrowser extends SwatControl
 					'one photo', '%s photos', $photos[$key]),
 					SwatString::numberFormat($photos[$key]));
 
-				if ($start_date->getDay() == $end_date->getDay() &&
-					$start_date->getDay() == $i) {
+				if ($selected_day === $i) {
 					$a_tag->class = 'selected';
 				}
 
-				$a_tag->href = 'tag?'.$tag_list->__toString();
+				$a_tag->href = $this->base.'?'.$tag_list->__toString();
 
 				$a_tag->setContent($date->format('%d'));
 				$a_tag->display();
@@ -234,12 +380,25 @@ class PinholeDateTagBrowser extends SwatControl
 		$div_tag->close();
 	}
 
-	protected function getPhotoCountByDate($date_part = 'day',
-		$tag_list = null)
-	{
-		if ($tag_list === null)
-			$tag_list = $this->tag_list;
+	// }}}
+	// {{{ protected function getPhotoCountByDate()
 
+	/**
+	 * Gets a summary of the number of photos in the specified tag list indexed
+	 * and grouped by the specified date part
+	 *
+	 * @param PinholeTagList $tag_list the tag list to get the photo count from.
+	 * @param string $date_part the date part with which to index and group
+	 *                           photo counts.
+	 *
+	 * @return array an array indexed by the relevant date part with values
+	 *                indicating the number of photos in the specified tag list
+	 *                for the date part index. If the tag list has no photos
+	 *                on a specific date, the returned array does not contain
+	 *                an index at that date.
+	 */
+	protected function getPhotoCountByDate(PinholeTagList $tag_list, $date_part)
+	{
 		$group_by_parts = array();
 
 		switch ($date_part) {
@@ -287,12 +446,9 @@ class PinholeDateTagBrowser extends SwatControl
 		if (strlen($join_clauses) > 0)
 			$sql.= ' '.$join_clauses.' ';
 
-		$sql.= sprintf(' where PinholePhoto.instance = %s',
-			$this->db->quote($this->instance->id, 'integer'));
-
 		$where_clause = $tag_list->getWhereClause();
 		if (strlen($where_clause) > 0)
-			$sql.= ' and '.$where_clause;
+			$sql.= ' where '.$where_clause;
 
 		if (strlen($group_by_clause) > 0)
 			$sql.= ' group by '.$group_by_clause;
@@ -301,13 +457,14 @@ class PinholeDateTagBrowser extends SwatControl
 
 		$dates = array();
 		while ($row = $rows->fetchRow(MDB2_FETCHMODE_OBJECT)) {
-			//foreach ($rows as $row) {
 			$date = new SwatDate($row->photo_date);
 			$dates[$date->format($date_format)] = $row->photo_count;
 		}
 
 		return $dates;
 	}
+
+	// }}}
 }
 
 ?>

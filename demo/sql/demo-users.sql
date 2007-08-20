f636c04829af2fafc4fefde836486d67916109a0
Nathan Fredrickson <nathan@silverorange.com>
2007-08-20 18:23:48 +0000

    setup user group bindings
    git-svn-id: file:///Users/gauthierm/svnrepo/pinhole/trunk@19525 94490950-a86c-4493-9ccb-530f72933e47

--- demo/sql/demo-users.sql
+++ demo/sql/demo-users.sql
@@ -1,2 +1,8 @@
 insert into AdminUser (id, email, name, password, password_salt, enabled, force_change_password)
 values (1, 'demo@silverorange.com', 'Demo User', 'fe01ce2a7fbac8fafaed7c982a04e229', '', true, false);
+
+SELECT setval('adminuser_id_seq', max(id)) FROM AdminUser;
+
+-- default AdminUserAdminGroupBinding bindings
+insert into AdminUserAdminGroupBinding (usernum, groupnum)
+	select AdminUser.id, AdminGroup.id from AdminUser, AdminGroup;

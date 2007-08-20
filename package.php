a35ead8f8b3a91259f5091eef5bf9fa27a71621b
Nathan Fredrickson <nathan@silverorange.com>
2007-08-20 17:01:56 +0000

    prepare for release of 1.0.11
    svn commit r19488

--- package.php
+++ package.php
@@ -42,6 +42,7 @@ $package->setAPIStability('stable');
 $package->setNotes($notes);
 
 $package->addIgnore('package.php');
+$package->addIgnore('packages/');
 
 $package->addMaintainer('lead', 'nrf', 'Nathan Fredrickson', 'nathan@silverorange.com');
 $package->addMaintainer('lead', 'gauthierm', 'Mike Gauthier', 'mike@silverorange.com');

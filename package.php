48679cbdd4eead7b2998630e8149ba5844309454
Nathan Fredrickson <nathan@silverorange.com>
2007-08-20 17:40:27 +0000

    prepare for release of 1.0.15
    git-svn-id: file:///Users/gauthierm/svnrepo/pinhole/trunk@19508 94490950-a86c-4493-9ccb-530f72933e47

--- package.php
+++ package.php
@@ -2,7 +2,7 @@
 
 require_once 'PEAR/PackageFileManager2.php';
 
-$version = '1.0.14';
+$version = '1.0.15';
 $notes = <<<EOT
 see ChangeLog
 EOT;
@@ -50,13 +50,11 @@ $package->addReplacement('Pinhole/Pinhole.php', 'pear-config', '@DATA-DIR@', 'da
 
 $package->setPhpDep('5.1.5');
 $package->setPearinstallerDep('1.4.0');
-$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.0.13');
-$package->addPackageDepWithChannel('required', 'Site', 'pear.silverorange.com', '1.0.0');
+$package->addPackageDepWithChannel('required', 'Swat', 'pear.silverorange.com', '1.2.29');
+$package->addPackageDepWithChannel('required', 'Site', 'pear.silverorange.com', '1.0.37');
 $package->addPackageDepWithChannel('required', 'Date', 'pear.silverorange.com', '1.4.7so1');
 $package->addPackageDepWithChannel('required', 'MDB2', 'pear.php.net', '2.2.2');
-$package->addPackageDepWithChannel('required', 'Mail', 'pear.php.net', '1.1.10');
-$package->addPackageDepWithChannel('required', 'Mail_mime', 'pear.silverorange.com', '1.3.0so1');
-$package->addPackageDepWithChannel('required', 'XML_RPC2', 'pear.silverorange.com', '0.0.7so2');
+$package->addPackageDepWithChannel('required', 'XML_RPCAjax', 'pear.silverorange.com', '1.0.5');
 $package->generateContents();
 
 if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {

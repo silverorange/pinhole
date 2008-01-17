3831e5782e3adda51058b547fd204baef0bd6a26
Michael Gauthier <mike@silverorange.com>
2008-01-17 21:36:50 +0000

    Demo shouldn't include package config.
    git-svn-id: file:///Users/gauthierm/svnrepo/pinhole/trunk@26043 94490950-a86c-4493-9ccb-530f72933e47

--- demo/www/index.php
+++ demo/www/index.php
@@ -1,15 +1,5 @@
 <?php
 
-@include_once 'PackageConfig.php';
-if (class_exists('PackageConfig')) {
-	PackageConfig::addPackage('swat');
-	PackageConfig::addPackage('site', 'work-nrf');
-	PackageConfig::addPackage('pinhole', 'work-nrf');
-	PackageConfig::addPackage('yui');
-	PackageConfig::addPackage('recaptcha');
-	PackageConfig::addPath('/so/sites/veseys2/pear/lib');
-}
-
 require_once '../include/Application.php';
 
 $config_filename = dirname(__FILE__).'/../demo.ini';

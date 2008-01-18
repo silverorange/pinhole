ea0f959f6b1628996f354b2e23c58db040703689
Nathan Fredrickson <nathan@silverorange.com>
2008-01-18 01:05:07 +0000

    Modernize admin to use Site trunk.
    svn commit r26063

--- Pinhole/admin/PinholeAdminApplication.php
+++ Pinhole/admin/PinholeAdminApplication.php
@@ -2,6 +2,7 @@
 
 require_once 'Site/SiteMultipleInstanceModule.php';
 require_once 'Admin/AdminApplication.php';
+require_once 'Pinhole/Pinhole.php';
 require_once 'Pinhole/dataobjects/PinholeAdminUser.php';
 
 /**
@@ -13,6 +14,15 @@ require_once 'Pinhole/dataobjects/PinholeAdminUser.php';
  */
 class PinholeAdminApplication extends AdminApplication
 {
+	// {{{ public function __construct()
+
+	public function __construct($id, $filename)
+	{
+		parent::__construct($id, $filename);
+		$this->config->session->name.= 'admin';
+	}
+
+	// }}}
 	// {{{ protected function getDefaultModuleList()
 
 	/**
@@ -27,6 +37,71 @@ class PinholeAdminApplication extends AdminApplication
 		$modules['instance'] = 'SiteMultipleInstanceModule';
 
 		return $modules;
+	}
+
+	// }}}
+	// {{{ protected function addConfigDefinitions()
+
+	/**
+	 * Adds configuration definitions to the config module of this application
+	 *
+	 * @param SiteConfigModule $config the config module of this application to
+	 *                                  witch to add the config definitions.
+	 */
+	protected function addConfigDefinitions(SiteConfigModule $config)
+	{
+		parent::addConfigDefinitions($config);
+		$config->addDefinitions(Pinhole::getConfigDefinitions());
+	}
+
+	// }}}
+	// {{{ protected function configure()
+
+	/**
+	 * Configures modules of this application before they are initialized
+	 *
+	 * @param SiteConfigModule $config the config module of this application to
+	 *                                  use for configuration other modules.
+	 */
+	protected function configure(SiteConfigModule $config)
+	{
+		parent::configure($config);
+
+		$this->addComponentIncludePath('Pinhole/admin/components', 'Pinhole');
+		$this->addComponentIncludePath('Site/admin/components', 'Site');
+
+		if (isset($config->exceptions->log_location))
+			SwatException::setLogger(new SiteExceptionLogger(
+				$config->exceptions->log_location,
+				$config->exceptions->base_uri));
+
+		if (isset($config->errors->log_location))
+			SwatError::setLogger(new SiteErrorLogger(
+				$config->errors->log_location, $config->errors->base_uri));
+
+		SwatForm::$default_salt = $config->swat->form_salt;
+
+		$this->database->dsn = $config->database->dsn;
+		$this->setBaseUri($config->uri->base.'admin/');
+		$this->setSecureBaseUri($config->uri->secure_base.'admin/');
+		$this->cookie->setSalt($config->cookies->salt);
+		$this->default_time_zone =
+			new Date_TimeZone($config->date->time_zone);
+
+		$this->default_locale = $config->i18n->locale;
+	}
+
+	// }}}
+	// {{{ protected function initModules()
+
+	/**
+	 * Initializes all modules in this application
+	 */
+	protected function initModules()
+	{
+		parent::initModules();
+
+		$this->title = sprintf('%s Admin', $this->config->site->title);
 	}
 
 	// }}}

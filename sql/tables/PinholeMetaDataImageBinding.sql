01546c9b68e3ecdd6073b4168b82cf83ec8d2647
Nick Burka <nick@silverorange.com>
2008-01-23 16:00:31 +0000

    Move back a little bit from the last rename. Rather than using the Image table for all types of images, we're going to alter the SiteImage dataobject to allow for the use of alternate tables to be used, i.e. PinholePhoto for SiteImage, and PinholePhotoDimensionBinding for SiteImageDimensionBinding.
    svn commit r26385

--- sql/tables/PinholeMetaDataImageBinding.sql
+++
@@ -1,8 +0,0 @@
-create table PinholeMetaDataImageBinding (
-	image int not null references Image(id) on delete cascade,
-	meta_data int not null references PinholeMetaData(id) on delete cascade,
-	value varchar(255) not null,
-	primary key (image, meta_data)
-);
-
-CREATE INDEX PinholeMetaDataImageBinding_value_index ON PinholeMetaDataImageBinding(value);
--- sql/tables/PinholeTagImageBinding.sql
+++
@@ -1,5 +0,0 @@
-create table PinholeTagImageBinding (
-	image int not null references Image(id) on delete cascade,
-	tag int not null references PinholeTag(id) on delete cascade,
-	primary key (image, tag)
-);

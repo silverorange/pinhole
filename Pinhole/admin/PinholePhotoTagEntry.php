1ab4670fcdfd4f2ed6de6f6c8eb19667ff1aa5c8
Nick Burka <nick@silverorange.com>
2008-06-04 19:47:45 +0000

    Remove unused require
    git-svn-id: file:///Users/gauthierm/svnrepo/public/pinhole/trunk@33241 94490950-a86c-4493-9ccb-530f72933e47

--- Pinhole/admin/PinholePhotoTagEntry.php
+++ Pinhole/admin/PinholePhotoTagEntry.php
@@ -1,9 +1,7 @@
 <?php
-
 /* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
 
 require_once 'Site/SiteTagEntry.php';
-require_once 'Pinhole/PinholeTagList.php';
 require_once 'Pinhole/dataobjects/PinholeTagDataObjectWrapper.php';
 require_once 'Pinhole/tags/PinholeTag.php';
 

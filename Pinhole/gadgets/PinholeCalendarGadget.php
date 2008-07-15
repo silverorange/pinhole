977edf53461f87a54c8111baa97c2423f52d8d07
Nick Burka <nick@silverorange.com>
2008-07-15 02:15:52 +0000

    Refactor, and link the month name. - the next step with this widget is to allow back/forward browsing with ajax.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/pinhole/trunk@34886 94490950-a86c-4493-9ccb-530f72933e47

--- Pinhole/gadgets/PinholeCalendarGadget.php
+++ Pinhole/gadgets/PinholeCalendarGadget.php
@@ -14,43 +14,38 @@ require_once 'SwatI18N/SwatI18NLocale.php';
  */
 class PinholeCalendarGadget extends SiteGadget
 {
+	// {{{ protected properties
+
+	protected $date;
+
+	// }}}
+	// {{{ public function __construct()
+
+	public function __construct(SiteApplication $app,
+		SiteGadgetInstance $instance)
+	{
+		parent::__construct($app, $instance);
+
+		$this->date = new SwatDate();
+		$this->date->setDay(1);
+		$this->date->clearTime();
+	}
+
+	// }}}
 	// {{{ protected function displayContent()
 
 	protected function displayContent()
 	{
-		$date = new SwatDate();
-		$date->setDay(1);
-		$date->clearTime();
+		$a_tag = new SwatHtmlTag('a');
+		$a_tag->setContent($this->date->format('%B, %Y'));
+		$a_tag->href = sprintf('%stag?date.month=%s/date.year=%s',
+			$this->app->config->pinhole->path,
+			$this->date->getMonth(),
+			$this->date->getYear());
 
-		$sql = "select count(PinholePhoto.id) as photo_count,
-				date_part('day', max(convertTZ(PinholePhoto.photo_date,
-					PinholePhoto.photo_time_zone))) as photo_day
-			from PinholePhoto
-			inner join ImageSet on PinholePhoto.image_set = ImageSet.id
-			where ImageSet.instance %s %s and PinholePhoto.status = %s
-				and PinholePhoto.photo_date is not null
-				and convertTZ(PinholePhoto.photo_date,
-					PinholePhoto.photo_time_zone) >= %s
-			group by date_part('day', convertTZ(PinholePhoto.photo_date,
-				PinholePhoto.photo_time_zone))";
-
-		$sql = sprintf($sql,
-			SwatDB::equalityOperator($this->app->getInstanceId()),
-			$this->app->db->quote($this->app->getInstanceId(), 'integer'),
-			$this->app->db->quote(PinholePhoto::STATUS_PUBLISHED),
-			$this->app->db->quote($date->getDate(), 'date'));
-
-		$days = SwatDB::query($this->app->db, $sql);
-
-		$day_count = array();
-		foreach ($days as $day) {
-			$day_count[$day->photo_day] = $day->photo_count;
-		}
-
-		$start = ((-1) * ($date->getDayOfWeek())) + 1;
-
+		$day_count = $this->getPhotoCountPerDay();
 		$h4 = new SwatHtmlTag('h4');
-		$h4->setContent($date->format('%B, %Y'));
+		$h4->setContent((string) $a_tag, 'text/xml');
 		$h4->display();
 
 		echo '<table>';
@@ -69,7 +64,8 @@ class PinholeCalendarGadget extends SiteGadget
 
 		$locale = SwatI18NLocale::get();
 		$count = 0;
-		for ($i = $start; $i <= $date->getDaysInMonth(); $i++) {
+		$start = ((-1) * ($this->date->getDayOfWeek())) + 1;
+		for ($i = $start; $i <= $this->date->getDaysInMonth(); $i++) {
 			if ($i == $start)
 				echo '<tr>';
 			elseif ($count % 7 == 0)
@@ -81,8 +77,8 @@ class PinholeCalendarGadget extends SiteGadget
 						'<a href="%stag?date.date=%s-%s-%s" '.
 						'title="%s %s">%s</a></td>',
 						$this->app->config->pinhole->path,
-						$date->getYear(),
-						$date->getMonth(),
+						$this->date->getYear(),
+						$this->date->getMonth(),
 						$i,
 						$locale->formatNumber($day_count[$i]),
 						Pinhole::ngettext('Photo', 'Photos', $day_count[$i]),
@@ -98,32 +94,51 @@ class PinholeCalendarGadget extends SiteGadget
 		}
 
 		echo '</tr></table>';
+	}
 
-		/*
-		$locale = SwatI18NLocale::get();
+	// }}}
+	// {{{ protected function getPhotoCountPerDay()
 
-		foreach ($years as $year) {
-			$date = new SwatDate($year->photo_date);
-			$date->getYear();
+	protected function getPhotoCountPerDay()
+	{
+		$sql = "select count(PinholePhoto.id) as photo_count,
+				date_part('day', max(convertTZ(PinholePhoto.photo_date,
+					PinholePhoto.photo_time_zone))) as photo_day
+			from PinholePhoto
+			inner join ImageSet on PinholePhoto.image_set = ImageSet.id
+			where ImageSet.instance %s %s and PinholePhoto.status = %s
+				and PinholePhoto.photo_date is not null
+				and convertTZ(PinholePhoto.photo_date,
+					PinholePhoto.photo_time_zone) >= %s
+				and convertTZ(PinholePhoto.photo_date,
+					PinholePhoto.photo_time_zone) < %s
+			group by date_part('day', convertTZ(PinholePhoto.photo_date,
+				PinholePhoto.photo_time_zone))";
 
-			$div_tag = new SwatHtmlTag('div');
-			$div_tag->class = 'pinhole-date-browser-gadget-year';
-			$div_tag->open();
-
-			$a_tag = new SwatHtmlTag('a');
-			$a_tag->href = $this->app->config->pinhole->path.'tag?date.year='.
-				$date->getYear();
-
-			$a_tag->setContent($date->getYear());
-
-			$a_tag->display();
-
-			echo ' '.$locale->formatNumber($year->photo_count).' '.
-				Pinhole::ngettext('Photo', 'Photos', $year->photo_count);
-
-			$div_tag->close();
+		$end_date = clone $this->date;
+		if ($end_date->getMonth() == 12) {
+			$end_date->setMonth(1);
+			$end_date->setYear($end_date->getYear() + 1);
+		} else {
+			$end_date->setMonth($end_date->getMonth() + 1);
 		}
-		*/
+
+		$sql = sprintf($sql,
+			SwatDB::equalityOperator($this->app->getInstanceId()),
+			$this->app->db->quote($this->app->getInstanceId(), 'integer'),
+			$this->app->db->quote(PinholePhoto::STATUS_PUBLISHED),
+			$this->app->db->quote($this->date->getDate(), 'date'),
+			$this->app->db->quote($end_date->getDate(), 'date'));
+
+
+		$days = SwatDB::query($this->app->db, $sql);
+
+		$day_count = array();
+		foreach ($days as $day) {
+			$day_count[$day->photo_day] = $day->photo_count;
+		}
+
+		return $day_count;
 	}
 
 	// }}}

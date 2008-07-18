9563614c5476c9c372d0fe4075fd33f375e6225b
Nick Burka <nick@silverorange.com>
2008-07-18 16:07:18 +0000

    Make PinholeCalendarGadget all ajaxy
    svn commit r34985

---
+++ Pinhole/pages/PinholeCalendarGadgetServer.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once 'Site/pages/SiteXMLRPCServer.php';
+require_once 'Site/exceptions/SiteNotFoundException.php';
+require_once 'Pinhole/gadgets/PinholeCalendarGadget.php';
+
+/**
+ * Handles XML-RPC requests from the pinhole calendar gadget
+ *
+ * @package   Pinhole
+ * @copyright 2006-2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class PinholeCalendarGadgetServer extends SiteXMLRPCServer
+{
+	// {{{ protected properties
+
+	protected $date;
+
+	// }}}
+	// {{{ public function getCalendar()
+
+	/**
+	 * Returns an XHTML calendar with photo counts
+	 *
+	 * @param integer $year The year to display
+	 * @param integer $month The month to display
+	 *
+	 * @return string an XHTML calendar.
+	 */
+	public function getCalendar($year, $month)
+	{
+		$date = new SwatDate();
+		$date->setYear($year);
+		$date->setMonth($month);
+		$date->setDay(1);
+		$date->clearTime();
+
+		ob_start();
+		PinholeCalendarGadget::displayCalendarMonth($this->app, $date);
+		$response['calendar_month'] = ob_get_clean();
+
+		ob_start();
+		PinholeCalendarGadget::displayCalendarBody($this->app, $date);
+		$response['calendar_body'] = ob_get_clean();
+
+		return $response;
+	}
+
+	// }}}
+}
+
+?>
---
+++ www/javascript/pinhole-calendar-gadget.js
@@ -0,0 +1,93 @@
+/**
+ * Controls the asynchronous loading of months for the calendar-gadget
+ *
+ * @package   Pinhole
+ * @copyright 2008 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+
+/**
+ * Creates a new CalendarGadget
+ *
+ * @param String id
+ * @param Integer year
+ * @param Integer month
+ */
+function PinholeCalendarGadget(id, year, month)
+{
+	this.id = id;
+	this.year = year;
+	this.month = month;
+
+	this.next_link = document.getElementById(id + '_next');
+	this.prev_link = document.getElementById(id + '_prev');
+	this.calendar_body = document.getElementById(id + '_body');
+	this.calendar_month = document.getElementById(id + '_month');
+	this.client = new XML_RPC_Client('xml-rpc/calendar-gadget');
+
+	this.cached_month = {};
+	this.cached_body = {};
+
+	this.cached_month[this.getCacheKey()] = this.calendar_month.innerHTML;
+	this.cached_body[this.getCacheKey()] = this.calendar_body.innerHTML;
+
+	YAHOO.util.Event.addListener(this.next_link, 'click', this.getNextMonth,
+		this, true);
+
+	YAHOO.util.Event.addListener(this.prev_link, 'click', this.getPrevMonth,
+		this, true);
+}
+
+PinholeCalendarGadget.prototype.getNextMonth = function()
+{
+	if (this.month == 12) {
+		this.month = 1;
+		this.year++;
+	} else {
+		this.month++;
+	}
+
+	this.loadCalendar();
+}
+
+PinholeCalendarGadget.prototype.getPrevMonth = function()
+{
+	if (this.month == 1) {
+		this.month = 12;
+		this.year--;
+	} else {
+		this.month--;
+	}
+
+	this.loadCalendar();
+}
+
+PinholeCalendarGadget.prototype.loadCalendar = function()
+{
+	var self = this;
+
+	var cache_key = this.getCacheKey();
+
+	function callBack(response)
+	{
+		self.calendar_month.innerHTML = response['calendar_month'];
+		self.calendar_body.innerHTML = response['calendar_body'];
+
+		self.cached_month[cache_key] = response['calendar_month'];
+		self.cached_body[cache_key] = response['calendar_body'];
+	}
+
+	if (this.cached_month[cache_key] && this.cached_body[cache_key]) {
+		self.calendar_month.innerHTML = this.cached_month[cache_key];
+		self.calendar_body.innerHTML = this.cached_body[cache_key];
+	} else {
+		this.client.callProcedure('getCalendar', callBack,
+			[this.year, this.month],
+			['int', 'int']);
+	}
+}
+
+PinholeCalendarGadget.prototype.getCacheKey = function()
+{
+	return this.year + '_' + this.month;
+}

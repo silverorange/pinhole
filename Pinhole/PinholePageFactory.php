0c624a7d51017592d209b2fb1a409ac3ec2a6f00
Michael Gauthier <mike@silverorange.com>
2008-07-23 20:18:13 +0000

    Update pinhole page factory to new API.
    git-svn-id: file:///Users/gauthierm/svnrepo/public/pinhole/trunk@35210 94490950-a86c-4493-9ccb-530f72933e47

--- Pinhole/PinholePageFactory.php
+++ Pinhole/PinholePageFactory.php
@@ -16,57 +16,115 @@ class PinholePageFactory extends SitePageFactory
 	/**
 	 * Creates a PinholePageFactory
 	 */
-	public function __construct()
+	public function __construct(SiteApplication $app)
 	{
+		parent::__construct($app);
+
 		// set location to load Pinhole page classes from
-		$this->class_map['Pinhole'] = 'Pinhole/pages';
+		$this->page_class_map['Pinhole'] = 'Pinhole/pages';
 	}
 
 	// }}}
-	// {{{ public function resolvePage()
+	// {{{ public function get()
 
 	/**
 	 * Resolves a page object from a source string
 	 *
-	 * @param SiteWebApplication $app the web application for which the page is
-	 *                               being resolved.
 	 * @param string $source the source string for which to get the page.
 	 * @param SiteLayout $layout optional, the layout to use.
 	 *
 	 * @return SitePage the page for the given source string.
 	 */
-	public function resolvePage(SiteWebApplication $app, $source,
-		$layout = null)
+	public function get($source, SiteLayout $layout = null)
 	{
-		if ($layout === null)
-			$layout = $this->resolveLayout($app, $source);
+		$layout = ($layout === null) ? $this->getLayout($source) : $layout;
 
 		if ($source == '')
 			$source = 'tag';
 
-		$article_path = $source;
+		$page_info = $this->getPageInfo($source);
 
-		$page = null;
+		if ($page_info === null) {
+			throw new SiteNotFoundException();
+		}
+
+		// create page object
+		$page = $this->getPage($page_info['page'], $layout,
+			$page_info['arguments']);
+
+		// decorate page
+		$decorators = array_reverse($page_info['decorators']);
+		foreach ($decorators as $decorator) {
+			$page = $this->decorate($page, $decorator);
+		}
+
+		return $page;
+	}
+
+	// }}}
+	// {{{ protected function getPageInfo()
+
+	/**
+	 * Gets page info for the passed source string
+	 *
+	 * @param string $source the source string for which to get the page info.
+	 *
+	 * @return array an array of page info. The array has the index values
+	 *               'page', 'path', 'decorators' and 'arguments'. If no
+	 *               suitable page is found, null is returned.
+	 */
+	protected function getPageInfo($source)
+	{
+		$info = null;
 
 		foreach ($this->getPageMap() as $pattern => $class) {
 			$regs = array();
 			$pattern = str_replace('@', '\@', $pattern); // escape delimiters
 			$regexp = '@'.$pattern.'@u';
 			if (preg_match($regexp, $source, $regs) === 1) {
-				array_shift($regs); // discard full match string
-				$path = array_shift($regs);
-				array_unshift($regs, $layout);
-				array_unshift($regs, $app);
 
-				$page = $this->instantiatePage($app, $class, $regs);
+				$info = array(
+					'page'       => $this->default_page_class,
+					'path'       => $source,
+					'decorators' => array(),
+					'arguments'  => array(),
+				);
+
+				array_shift($regs); // discard full match string
+
+				// get path as first subpattern
+				$info['path'] = array_shift($regs);
+
+				// get additional arguments as remaining subpatterns
+				foreach ($regs as $reg) {
+					// set empty regs parsed from page map expressions to null
+					$reg = ($reg == '') ? null : $reg;
+					$info['arguments'][] = $reg;
+				}
+
+				// get page class and/or decorators
+				if (is_array($class)) {
+					$page = array_pop($class);
+					if ($this->isPage($page)) {
+						$info['page']       = $page;
+						$info['decorators'] = $class;
+					} else {
+						$class[]            = $page;
+						$info['decorators'] = $class;
+					}
+				} else {
+					if ($this->isPage($class)) {
+						$info['page'] = $class;
+					} else {
+						$info['decorators'][] = $class;
+					}
+				}
+
 				break;
 			}
 		}
 
-		if ($page === null)
-			throw new SiteNotFoundException();
-
-		return $page;
+		return $info;
 	}
 
 	// }}}
@@ -75,19 +133,19 @@ class PinholePageFactory extends SitePageFactory
 	protected function getPageMap()
 	{
 		return array(
-			'^(photo)/([0-9]+)/([\w\d*]+)$'   => 'PinholeBrowserDetailsPage',
-			'^(photo)/([0-9]+)$'              => 'PinholeBrowserDetailsPage',
-			'^(tag)$'                         => 'PinholeBrowserIndexPage',
-			'^(tags)$'                        => 'PinholeBrowserTagPage',
-			'^(tags)/(date)$'                 => 'PinholeBrowserTagPage',
-			'^(tags)/(alphabetical)$'         => 'PinholeBrowserTagPage',
-			'^(tags)/(popular)$'              => 'PinholeBrowserTagPage',
-			'^(tags)/(cloud)$'                => 'PinholeBrowserTagPage',
-			'^(feed)$'                        => 'PinholeAtomPage',
-			'^(feed)/([\w\d*]+)$'             => 'PinholeAtomPage',
-			'^(loadphoto)/(.+)/(.+).jpg$'     => 'PinholePhotoLoaderPage',
-			'^(login)$'                       => 'PinholeLoginPage',
-			'^robots.txt$'                    => 'PinholeRobotsPage',
+			'^(photo)/(\d+)/([\w\d*]+)$'  => 'PinholeBrowserDetailsPage',
+			'^(photo)/(\d+)$'             => 'PinholeBrowserDetailsPage',
+			'^(tag)$'                     => 'PinholeBrowserIndexPage',
+			'^(tags)$'                    => 'PinholeBrowserTagPage',
+			'^(tags)/(date)$'             => 'PinholeBrowserTagPage',
+			'^(tags)/(alphabetical)$'     => 'PinholeBrowserTagPage',
+			'^(tags)/(popular)$'          => 'PinholeBrowserTagPage',
+			'^(tags)/(cloud)$'            => 'PinholeBrowserTagPage',
+			'^(feed)$'                    => 'PinholeAtomPage',
+			'^(feed)/([\w\d*]+)$'         => 'PinholeAtomPage',
+			'^(loadphoto)/(.+)/(.+).jpg$' => 'PinholePhotoLoaderPage',
+			'^(login)$'                   => 'PinholeLoginPage',
+			'^robots.txt$'                => 'PinholeRobotsPage',
 		);
 	}
 

4456d0950569919a2d5bd9733d9cffb8c2d35dee
Nick Burka <nick@silverorange.com>
2008-07-29 00:00:14 +0000

    Improved date-browsing gadget
    svn commit r35432

--- Pinhole/gadgets/PinholeDateBrowserGadget.php
+++ Pinhole/gadgets/PinholeDateBrowserGadget.php
@@ -2,6 +2,8 @@
 
 require_once 'Site/SiteGadget.php';
 require_once 'Swat/SwatString.php';
+require_once 'Swat/SwatDisclosure.php';
+require_once 'Swat/SwatContentBlock.php';
 require_once 'Swat/SwatHtmlTag.php';
 require_once 'SwatI18N/SwatI18NLocale.php';
 
@@ -14,6 +16,11 @@ require_once 'SwatI18N/SwatI18NLocale.php';
  */
 class PinholeDateBrowserGadget extends SiteGadget
 {
+	// {{{ private properties
+
+	private $container;
+
+	// }}}
 	// {{{ protected function displayContent()
 
 	protected function displayContent()
@@ -26,6 +33,8 @@ class PinholeDateBrowserGadget extends SiteGadget
 			where ImageSet.instance %s %s and PinholePhoto.status = %s
 				and PinholePhoto.photo_date is not null
 			group by date_part('year', convertTZ(PinholePhoto.photo_date,
+				PinholePhoto.photo_time_zone)),
+				date_part('month', convertTZ(PinholePhoto.photo_date,
 				PinholePhoto.photo_time_zone))
 			order by photo_date desc";
 
@@ -34,33 +43,102 @@ class PinholeDateBrowserGadget extends SiteGadget
 			$this->app->db->quote($this->app->getInstanceId(), 'integer'),
 			$this->app->db->quote(PinholePhoto::STATUS_PUBLISHED, 'integer'));
 
-		$years = SwatDB::query($this->app->db, $sql);
-		$locale = SwatI18NLocale::get();
+		$months = SwatDB::query($this->app->db, $sql);
 
-		echo '<ul>';
-
-		foreach ($years as $year) {
-			$date = new SwatDate($year->photo_date);
-			$date->getYear();
-
-			$li_tag = new SwatHtmlTag('li');
-			$li_tag->open();
-
-			$a_tag = new SwatHtmlTag('a');
-			$a_tag->href = $this->app->config->pinhole->path.'tag?date.year='.
-				$date->getYear();
-
-			$a_tag->setContent($date->getYear());
-
-			$a_tag->display();
-
-			echo ' '.$locale->formatNumber($year->photo_count).' '.
-				Pinhole::ngettext('Photo', 'Photos', $year->photo_count);
-
-			$li_tag->close();
+		if (count($months) == 0) {
+			echo Pinhole::_('No photos have been uploaded yet.');
+			return;
 		}
 
-		echo '</ul>';
+		$months_array = array();
+
+		foreach ($months as $month) {
+			$date = new SwatDate($month->photo_date);
+			$key = $date->getYear().'/'.$date->getMonth();
+			$months_array[$key] = $month;
+		}
+
+		$this->container = new SwatContainer();
+		$locale = SwatI18NLocale::get();
+
+		$start_date = new SwatDate($months->getFirst()->photo_date);
+		$start_year = $start_date->getYear();
+
+		$index = (count($months) - 1);
+		$end_date = new SwatDate($months->getByIndex($index)->photo_date);
+		$end_year = $end_date->getYear();
+
+		$date = new SwatDate();
+
+		for ($year = $start_year; $year >= $end_year; $year--) {
+			$year_count = 0;
+
+			$disclosure = new SwatDisclosure();
+			$disclosure->title = $year;
+			$disclosure->open = false;
+
+			ob_start();
+
+			echo '<ul>';
+
+			for ($i = 1; $i <= 12; $i++) {
+				echo '<li class="clearfix"><div>';
+
+				$date->setMonth($i);
+
+				if (isset($months_array[$year.'/'.$i])) {
+					$a_tag = new SwatHtmlTag('a');
+					$a_tag->setContent($date->getMonthName());
+					$a_tag->href = $this->app->config->pinhole->path.
+						'tag?date.year='.$year.
+						'/date.month='.$i;
+
+					$a_tag->display();
+
+					$photo_count = $months_array[$year.'/'.$i]->photo_count;
+					echo '<span>'.$locale->formatNumber($photo_count).
+						'</span>';
+
+					$year_count += $photo_count;
+				} else {
+					$div_tag = new SwatHtmlTag('div');
+					$div_tag->setContent($date->getMonthName());
+					$div_tag->display();
+				}
+
+				echo '</div></li>';
+
+				if ($i == 12 && $year_count > 0) {
+					echo '<li class="clearfix"><div>';
+
+					$a_tag = new SwatHtmlTag('a');
+					$a_tag->setContent(sprintf(Pinhole::_(
+						'View all photos from %s'),
+						$year));
+
+					$a_tag->href = $this->app->config->pinhole->path.
+						'tag?date.year='.$year;
+
+					$a_tag->display();
+
+					echo '<span>'.$locale->formatNumber($year_count).
+						'</span>';
+
+					echo '</div></li>';
+				}
+			}
+
+			echo '</ul>';
+
+			$content = new SwatContentBlock();
+			$content->content_type = 'text/xml';
+			$content->content = ob_get_clean();
+			$disclosure->add($content);
+			$this->container->add($disclosure);
+		}
+
+		$this->container->display();
+		$this->html_head_entry_set->addEntrySet($this->container->getHtmlHeadEntrySet());
 	}
 
 	// }}}
